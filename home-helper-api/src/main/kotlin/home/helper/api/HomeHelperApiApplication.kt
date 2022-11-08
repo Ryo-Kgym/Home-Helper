981a08cfaa3f8574798771751a5d6cc526be0c40
Ryo-Kgym <mraytoh@icloud.com>
2022-11-08 22:47:36 +0900

    - Created the default api project.

---
+++ home-helper-api/src/main/kotlin/home/helper/api/HomeHelperApiApplication.kt
@@ -0,0 +1,11 @@
+package home.helper.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeHelperApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomeHelperApiApplication>(*args)
+}
