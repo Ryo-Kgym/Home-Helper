1d84be3ac9eb771e48e6586e07b53464e5720949
Ryo-Kgym <mraytoh@icloud.com>
2022-11-09 00:56:51 +0900

    - Created the SaveDefaultGateway.

---
+++ home-helper-core/src/main/kotlin/home/helper/core/dto/SaveOutput.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2022 Ryo-Kgym.
+ */
+
+package home.helper.core.dto
+
+/**
+ * 保存処理の実行結果を保持するクラスです。
+ */
+data class SaveOutput(
+    val result: Int,
+)
---
+++ home-helper-core/src/main/kotlin/home/helper/core/gateway/SaveDefaultGateway.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 Ryo-Kgym.
+ */
+
+package home.helper.core.gateway
+
+import home.helper.core.dto.SaveOutput
+
+/**
+ * 保存用のデフォルトゲートウェイ インターフェースです。
+ * @param T 対象データ型
+ */
+interface SaveDefaultGateway<T> {
+
+    /**
+     * 対象データを保存します。
+     * @param target 対象データ
+     * @return 実行結果
+     */
+    fun save(target: T): SaveOutput
+
+}
