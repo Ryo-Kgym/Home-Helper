5ba74483fa5f5a8f10d19b9edc173b0ce487077d
Ryo-Kgym <mraytoh@icloud.com>
2022-11-09 00:47:37 +0900

    - Created the RegisterHelpPointUseCase. - But, It is not implemented yet.

---
+++ home-helper-core/src/main/kotlin/home/helper/core/domain/interactor/help/RegisterHelpPointInteractor.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2022 Ryo-Kgym.
+ */
+
+package home.helper.core.domain.interactor.help
+
+import home.helper.core.dto.RegisterOutput
+import home.helper.core.domain.use_case.help.RegisterHelpPointUseCase
+import home.helper.core.dto.help.RegisterHelpPointInput
+
+class RegisterHelpPointInteractor : RegisterHelpPointUseCase {
+
+    override fun register(input: RegisterHelpPointInput): RegisterOutput {
+        TODO("Not yet implemented")
+    }
+
+}
