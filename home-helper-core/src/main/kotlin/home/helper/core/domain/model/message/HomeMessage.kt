cb6dbed59e398afdaf8c2d040ec45bc58d52749d
Ryo-Kgym <mraytoh@icloud.com>
2022-11-09 00:43:48 +0900

    - Created the HomeMessage.

---
+++ home-helper-core/src/main/kotlin/home/helper/core/domain/model/message/HomeMessage.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022 Ryo-Kgym.
+ */
+
+package home.helper.core.domain.model.message
+
+data class HomeMessage(
+    val message: String
+)
--- home-helper-core/src/main/kotlin/home/helper/core/domain/use_case/RegisterDefaultUseCase.kt
+++ home-helper-core/src/main/kotlin/home/helper/core/domain/use_case/RegisterDefaultUseCase.kt
@@ -4,6 +4,8 @@
 
 package home.helper.core.domain.use_case
 
+import home.helper.core.dto.RegisterOutput
+
 /**
  * 登録用のデフォルトユースケースのインターフェースです。
  * @param I InputDS
@@ -14,6 +16,6 @@ interface RegisterDefaultUseCase<I> {
      * 入力ソースを加工し、登録します。
      * @param input 入力ソース
      */
-    fun register(input: I): Int
+    fun register(input: I): RegisterOutput
 
 }
