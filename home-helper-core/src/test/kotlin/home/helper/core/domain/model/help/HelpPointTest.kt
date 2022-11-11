d24b09736ca9df5eb44c77bf73fd775cf0211b85
Ryo-Kgym <mraytoh@icloud.com>
2022-11-11 21:59:32 +0900

    - Added the sum method.

--- home-helper-core/src/test/kotlin/home/helper/core/domain/model/help/HelpPointTest.kt
+++ home-helper-core/src/test/kotlin/home/helper/core/domain/model/help/HelpPointTest.kt
@@ -6,11 +6,13 @@ package home.helper.core.domain.model.help
 
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 internal class HelpPointTest {
 
     @Test
+    @DisplayName("可変長引数")
     fun sum() {
         val actual = HelpPoint.sum(
             HelpPoint(100), HelpPoint(300),
@@ -22,6 +24,37 @@ internal class HelpPointTest {
     }
 
     @Test
+    @DisplayName("配列が引数")
+    fun sumList() {
+        val actual = HelpPoint.sum(
+            listOf(
+                HelpPoint(100), HelpPoint(300),
+                HelpPoint(500), HelpPoint(200),
+            )
+        )
+        val expected = HelpPoint(1100)
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    @DisplayName("お手伝い項目が引数")
+    fun sumItemList() {
+        val actual = HelpPoint.sumItem(
+            listOf(
+                HelpItem("アイテム1",100),
+                HelpItem("アイテム2",300),
+                HelpItem("アイテム3",500),
+                HelpItem("アイテム4",200),
+            )
+        )
+        val expected = HelpPoint(1100)
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    @DisplayName("お手伝いポイント同士の加算")
     fun add() {
         val actual = HelpPoint(123)
             .add(HelpPoint(456))
