f4e908c516ea107e16376bbe41590dbb92852afb
Ryo-Kgym <mraytoh@icloud.com>
2022-11-11 22:30:38 +0900

    - Tested the RegisterHelpPointInteractor.kt

--- home-helper-core/src/main/kotlin/home/helper/core/domain/interactor/help/RegisterHelpPointInteractor.kt
+++ home-helper-core/src/main/kotlin/home/helper/core/domain/interactor/help/RegisterHelpPointInteractor.kt
@@ -4,17 +4,26 @@
 
 package home.helper.core.domain.interactor.help
 
-import home.helper.core.dto.RegisterOutput
+import home.helper.core.domain.model.help.HelpPoint
 import home.helper.core.domain.use_case.help.RegisterHelpPointUseCase
+import home.helper.core.dto.RegisterOutput
 import home.helper.core.dto.help.RegisterHelpPointInput
+import home.helper.core.dto.help.RegisterHelpPointOutput
 import home.helper.core.gateway.SaveDefaultGateway
 
 class RegisterHelpPointInteractor(
-    private val helpPointSaveGateway: SaveDefaultGateway<RegisterHelpPointInput>
+    private val helpPointSaveGateway: SaveDefaultGateway<RegisterHelpPointOutput>
 ) : RegisterHelpPointUseCase {
 
     override fun register(input: RegisterHelpPointInput): RegisterOutput {
-        val saveOutput = helpPointSaveGateway.save(input)
+        val totalHelpPoint = HelpPoint.sumItem(input.helpItemList)
+
+        val saveOutput = helpPointSaveGateway.save(
+            RegisterHelpPointOutput(
+                helpItemList = input.helpItemList,
+                totalHelpPoint = totalHelpPoint,
+            )
+        )
 
         return RegisterOutput()
     }
