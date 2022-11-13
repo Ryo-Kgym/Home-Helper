9b68d08032c81e359a183a3804125ef95c7279fb
Ryo-Kgym <mraytoh@icloud.com>
2022-11-13 09:34:49 +0900

    - Created the RegisterHelpItemUseCase.kt.

---
+++ home-helper-core/src/main/kotlin/home/helper/core/domain/use_case/help/RegisterHelpItemUseCase.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2022 Ryo-Kgym.
+ */
+
+package home.helper.core.domain.use_case.help
+
+import home.helper.core.domain.use_case.RegisterDefaultUseCase
+import home.helper.core.dto.help.RegisterHelpItemInput
+
+interface RegisterHelpItemUseCase: RegisterDefaultUseCase<RegisterHelpItemInput>
