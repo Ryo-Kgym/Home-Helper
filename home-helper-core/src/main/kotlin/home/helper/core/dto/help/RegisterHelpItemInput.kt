e3e694cba62fa4f33497a77d27d946c49f9d18aa
Ryo-Kgym <mraytoh@icloud.com>
2022-11-13 10:11:35 +0900

    - Changed the data type.

--- home-helper-core/src/main/kotlin/home/helper/core/dto/help/RegisterHelpItemInput.kt
+++ home-helper-core/src/main/kotlin/home/helper/core/dto/help/RegisterHelpItemInput.kt
@@ -9,5 +9,5 @@ import home.helper.core.domain.model.help.HelpPoint
 data class RegisterHelpItemInput(
     val helpItemName: String,
     val helpPoint: HelpPoint,
-    val memo: String,
+    val memo: String?,
 )
