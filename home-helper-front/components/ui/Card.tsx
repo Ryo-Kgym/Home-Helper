59bcda4527b1e8637b9d0ed0ecda29820463b9ef
Ryo-Kgym <mraytoh@icloud.com>
2022-11-20 01:04:16 +0900

    - Created the card link component.

--- home-helper-front/components/ui/Card.tsx
+++ home-helper-front/components/ui/Card.tsx
@@ -1,17 +1,43 @@
 import Link from "next/link";
 import styles from "../../styles/Home.module.css";
 
-const Card = (props: {
-    href: string,
-    label: string,
-}) => {
+const Card = (props: CardProps) => {
+    const label = props.back
+        ? <h2>&larr; {props.label}</h2>
+        : <h2> {props.label} &rarr;</h2>
+
     return (
         <>
             <Link href={props.href} className={styles.card}>
-                <h2>{props.label} &rarr;</h2>
+                {label}
             </Link>
         </>
     )
 }
 
 export default Card
+
+export const CardLinkList = (props: {
+    pageList: CardProps[]
+}) => {
+    return (
+        <div className={styles.grid}>
+            {props.pageList.map((e, i) => {
+                return (
+                    <Card
+                        href={e.href}
+                        label={e.label}
+                        back={e.back}
+                        key={'card' + i}
+                    />
+                )
+            })}
+        </div>
+    )
+}
+
+export interface CardProps {
+    href: string
+    label: string
+    back?: boolean
+}
