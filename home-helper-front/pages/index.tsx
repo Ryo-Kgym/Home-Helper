08c05f6acd00407b8abcc1b58bab89b3594479ee
Ryo-Kgym <mraytoh@icloud.com>
2022-11-19 23:40:47 +0900

    - Separated the components.

--- home-helper-front/pages/index.tsx
+++ home-helper-front/pages/index.tsx
@@ -1,30 +1,56 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from "next/link";
+import Card from "../components/ui/Card";
 
-export default function Home() {
+const pageList = [
+    {href: '/homeHelper', label: 'お手伝いアプリ'},
+    {href: '/household', label: '家計簿アプリ'},
+]
+
+export default function index() {
     return (
         <div className={styles.container}>
-            <Head>
-                <title>うちのアプリ</title>
-                <meta name="description" content="これは、うちの支援アプリです。"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+            <HomeHeader/>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>
-                    うちのアプリへようこそ！
-                </h1>
-
-                <div className={styles.grid}>
-                    <Link href={'/homeHelper'} className={styles.card}>
-                        <h2>お手伝いアプリ &rarr;</h2>
-                    </Link>
-                    <Link href={'/household'} className={styles.card}>
-                        <h2>家計簿アプリ &rarr;</h2>
-                    </Link>
-                </div>
+                <HomeTitle/>
+                <HomeLink/>
             </main>
         </div>
+    )
+}
+
+const HomeHeader = () => {
+    return (
+        <Head>
+            <title>うちのアプリ</title>
+            <meta name="description" content="これは、うちの支援アプリです。"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+    )
+}
+
+const HomeTitle = () => {
+    return (
+        <h1 className={styles.title}>
+            うちのアプリへようこそ！
+        </h1>
+    )
+}
+
+const HomeLink = () => {
+    return (
+        <div className={styles.grid}>
+            {pageList.map((e, i) => {
+                return (
+                    <Card
+                        href={e.href}
+                        label={e.label}
+                        key={'card' + i}
+                    />
+                )
+            })}
+        </div>
+
     )
 }
