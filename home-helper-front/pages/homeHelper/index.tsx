3639d5a4c6738ab9bd19c8709b9f0d7d20cc1547
Ryo-Kgym <mraytoh@icloud.com>
2022-11-19 23:18:32 +0900

    - Added the sub page.

---
+++ home-helper-front/pages/homeHelper/index.tsx
@@ -0,0 +1,5 @@
+export default function index() {
+    return (
+        <h2>お手伝いアプリ</h2>
+    )
+}
---
+++ home-helper-front/pages/household/index.tsx
@@ -0,0 +1,5 @@
+export default function index() {
+    return (
+        <h2>家計簿あぷり</h2>
+    )
+}
