5eff67e1949279444dec9618ec742e645400f785
Ryo-Kgym <mraytoh@icloud.com>
2022-11-29 00:45:22 +0900

    - Changed the structure.

--- home-helper-front/pages/homeHelper/chargePoint.tsx
+++ home-helper-front/pages/homeHelper/chargePoint.tsx
@@ -1,23 +1,6 @@
 import { Page } from "@components/page/charge_point/ChargePointPage";
 
-const index = (props: any) => {
-  return <Page data={props.data} />;
+const index = () => {
+  return <Page />;
 };
 export default index;
-
-export async function getServerSideProps() {
-  const helpItems = [
-    { id: 1, point: 100, name: "お皿運び" },
-    { id: 2, point: 200, name: "お風呂掃除" },
-    { id: 3, point: 300, name: "トイレ掃除" },
-    { id: 4, point: 50, name: "水やり" },
-    { id: 5, point: 10, name: "お片付け" },
-  ];
-
-  const data = {
-    fromDate: "11月13日",
-    helpItems: helpItems,
-  };
-
-  return { props: { data } };
-}
