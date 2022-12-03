02adb8751f3b224f03012cc2ba0dc5ef99b7300a
Ryo-Kgym <mraytoh@icloud.com>
2022-12-03 23:55:50 +0900

    - Added the helpPointEarnedAchievementIdGateway.kt.

--- home-helper-api/src/main/kotlin/home/helper/api/config/IdConfig.kt
+++ home-helper-api/src/main/kotlin/home/helper/api/config/IdConfig.kt
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import home.helper.api.gateway.id.IdGateway
 import home.helper.api.persistence.database.mysql.id.HelpItemIdRepository
+import home.helper.api.persistence.database.mysql.id.HelpPointEarnedAchievementId
+import home.helper.api.persistence.database.mysql.id.HelpPointEarnedAchievementIdRepository
 import home.helper.api.persistence.database.mysql.id.IdType
 import home.helper.api.persistence.database.mysql.id.RandomIdRepository
 import home.helper.api.persistence.database.mysql.table_customize.IdHolderCustomRepository
@@ -25,14 +27,29 @@ class IdConfig {
     fun helpItemIdGateway(@Value("\${mock.id::false}") mockId: Boolean,
                           idHolderCustomRepository: IdHolderCustomRepository
     ): IdGateway<HelpItemId> {
-        if (mockId) {
-            return object : RandomIdRepository<HelpItemId>(IdType.HELP_ITEM_ID) {
+        return if (mockId) {
+            HelpItemIdRepository(idHolderCustomRepository)
+        } else {
+            object : RandomIdRepository<HelpItemId>(IdType.HELP_ITEM_ID) {
                 override fun generateId(id: String): HelpItemId {
                     return HelpItemId(id)
                 }
             }
         }
 
-        return HelpItemIdRepository(idHolderCustomRepository)
+    }
+
+    @Bean
+    fun helpPointEarnedAchievementIdGateway(@Value("\${mock.id::false}") mockId: Boolean,
+                                            idHolderCustomRepository: IdHolderCustomRepository
+    ): IdGateway<HelpPointEarnedAchievementId> {
+        return if (mockId) {
+            HelpPointEarnedAchievementIdRepository(idHolderCustomRepository)
+        } else object : RandomIdRepository<HelpPointEarnedAchievementId>
+            (IdType.HELP_POINT_EARNED_ACHIEVEMENT_ID) {
+            override fun generateId(id: String): HelpPointEarnedAchievementId {
+                return HelpPointEarnedAchievementId(id)
+            }
+        }
     }
 }
