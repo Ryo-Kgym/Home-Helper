6a04961942db361d75a45bdf3583415b8ed4dc5e
Ryo-Kgym <mraytoh@icloud.com>
2022-12-10 23:51:43 +0900

    - Refactored the structure.

--- home-helper-front/components/container/possession_point/PossessionPointContainer.tsx
+++ home-helper-front/components/container/possession_point/PossessionPointContainer.tsx
@@ -1,24 +1,19 @@
 import { FC, useEffect, useState } from "react";
 import { PossessionPointPresenter } from "@components/presenter/possession_point/PossessionPointPresenter";
-import { User } from "@domain/model/home_helper/User";
 import { fetchUser } from "@hooks/user/fetchUser";
-import { loadUserId } from "@hooks/loadUserId";
 
 export const PossessionPointContainer: FC = () => {
-  const [user, setUser] = useState<User>({
-    id: "",
-    name: "",
-    currentPoint: 0,
-    lastHelp: new Date(),
-  });
+  const [currentPoint, setCurrentPoint] = useState(0);
+  const [userName, setUserName] = useState("");
 
   useEffect(() => {
     fetchUser().then((user) => {
-      setUser(user);
+      setCurrentPoint(user.currentPoint);
+      setUserName(user.name);
     });
-  }, []);
+  });
 
   return (
-    <PossessionPointPresenter name={user.name} point={user.currentPoint} />
+    <PossessionPointPresenter userName={userName} currentPoint={currentPoint} />
   );
 };
--- home-helper-front/components/presenter/possession_point/PossessionPointPresenter.tsx
+++ home-helper-front/components/presenter/possession_point/PossessionPointPresenter.tsx
@@ -8,8 +8,8 @@ import Image from "next/image";
 import piggy from "@public/piggy_bank.svg";
 
 type PossessionPointPresenterProps = {
-  name: string;
-  point: number;
+  userName: string;
+  currentPoint: number;
 };
 
 const cardListProps = [
@@ -19,14 +19,14 @@ const cardListProps = [
 ];
 
 export const PossessionPointPresenter: FC<PossessionPointPresenterProps> = ({
-  name,
-  point,
+  userName,
+  currentPoint,
 }) => {
   return (
     <Layout
       main={
         <>
-          <Point name={name} point={point} />
+          <Point name={userName} point={currentPoint} />
           <LinkList props={cardListProps} />
         </>
       }
