65b2385a1803ab7240015f75eced0269a7f0f920
Ryo-Kgym <mraytoh@icloud.com>
2022-12-10 23:27:37 +0900

    - Used the saveUserId.

--- home-helper-front/components/container/home_helper/HomeHelperContainer.tsx
+++ home-helper-front/components/container/home_helper/HomeHelperContainer.tsx
@@ -2,12 +2,13 @@ import { FC, useEffect, useState } from "react";
 import { HomeHelperPresenter } from "@components/presenter/home_helper/HomeHelperPresenter";
 import { fetchUsers } from "@hooks/user/fetchUsers";
 import { User } from "@domain/model/home_helper/User";
+import { saveUserId } from "@hooks/loadUserId";
 
 export const HomeHelperContainer: FC = () => {
   const [users, setUsers] = useState<User[]>([]);
 
   const handleClickUser = (userId: string) => {
-    sessionStorage.setItem("userId", userId);
+    saveUserId(userId);
   };
 
   useEffect(() => {
