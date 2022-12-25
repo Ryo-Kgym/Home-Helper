95bab1da7e4753f7210d3d2d4d1509a9d16c411d
Ryo-Kgym <mraytoh@icloud.com>
2022-12-25 23:01:41 +0900

    - Added the calcTotalPoint method.

--- home-helper-core/src/main/kotlin/home/helper/core/domain/model/exchange_item/ExchangePoint.kt
+++ home-helper-core/src/main/kotlin/home/helper/core/domain/model/exchange_item/ExchangePoint.kt
@@ -11,8 +11,31 @@ data class ExchangePoint(
     val point: Int
 ) {
     companion object {
+        /**
+         * 交換品のポイントを生成します。
+         * @param point ポイント
+         * @return 交換品のポイント
+         */
         fun valueOf(point: Int): ExchangePoint {
             return ExchangePoint(point)
+        }
+
+        /**
+         * 交換品のポイントの合計を算出します。
+         * @param multipleExchangeItems 複数の交換品
+         * @return 交換品のポイントの合計
+         */
+        fun calcTotalPoint(vararg multipleExchangeItems: MultipleExchangeItem): ExchangePoint {
+            return calcTotalPoint(multipleExchangeItems.toList())
+        }
+
+        /**
+         * 交換品のポイントの合計を算出します。
+         * @param multipleExchangeItems 複数の交換品
+         * @return 交換品のポイントの合計
+         */
+        fun calcTotalPoint(multipleExchangeItems: List<MultipleExchangeItem>): ExchangePoint {
+            return valueOf(multipleExchangeItems.sumOf { it.calcSubtotalPoint().point })
         }
     }
 
--- home-helper-core/src/test/kotlin/home/helper/core/domain/model/exchange_item/ExchangePointTest.kt
+++ home-helper-core/src/test/kotlin/home/helper/core/domain/model/exchange_item/ExchangePointTest.kt
@@ -8,9 +8,31 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
-
 internal class ExchangePointTest {
+
+    @Test
+    internal fun calcTotalPointVararg() {
+        val actual = ExchangePoint.calcTotalPoint(
+            MultipleExchangeItem("Id1", "name1", 100, 2),
+            MultipleExchangeItem("Id2", "name2", 200, 3),
+        )
+        val expected = ExchangePoint.valueOf(800)
+
+        assertThat(actual, `is`(expected))
+    }
+
+    @Test
+    internal fun calcTotalPoint() {
+        val actual = ExchangePoint.calcTotalPoint(
+            listOf(
+                MultipleExchangeItem("Id1", "name1", 100, 2),
+                MultipleExchangeItem("Id2", "name2", 200, 3),
+            )
+        )
+        val expected = ExchangePoint.valueOf(800)
+
+        assertThat(actual, `is`(expected))
+    }
 
     @Test
     fun times() {
