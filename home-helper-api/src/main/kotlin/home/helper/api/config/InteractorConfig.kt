6f9bc551ff3be866406c6b5b3f0ebcb4aac37775
Ryo-Kgym <mraytoh@icloud.com>
2022-12-25 23:05:14 +0900

    - Added the exchangeHelpPointUseCase bean.

--- home-helper-api/src/main/kotlin/home/helper/api/config/InteractorConfig.kt
+++ home-helper-api/src/main/kotlin/home/helper/api/config/InteractorConfig.kt
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import home.helper.api.persistence.database.mysql.help_item.register.RegisterHelpItemSaveRepository
+import home.helper.api.persistence.database.mysql.help_point.exchange.ExchangeHelpPointSaveRepository
 import home.helper.api.persistence.database.mysql.help_point.register.RegisterHelpPointRepository
 import home.helper.core.domain.interactor.help_item.RegisterHelpItemInteractor
+import home.helper.core.domain.interactor.help_point.ExchangeHelpPointInteractor
 import home.helper.core.domain.interactor.help_point.RegisterHelpPointInteractor
 import home.helper.core.domain.use_case.help_item.RegisterHelpItemUseCase
+import home.helper.core.domain.use_case.help_point.ExchangeHelpPointUseCase
 import home.helper.core.domain.use_case.help_point.RegisterHelpPointUseCase
 import home.helper.core.gateway.message.SaveMessageGateway
 import home.helper.core.gateway.operation.OperationGateway
@@ -43,6 +46,19 @@ class InteractorConfig {
         return RegisterHelpPointInteractor(
             operationGateway = operationGateway,
             helpPointSaveGateway = helpPointSaveGateway,
+            saveMessageGateway = saveMessageGateway,
+        )
+    }
+
+    @Bean
+    fun exchangeHelpPointUseCase(
+        operationGateway: OperationGateway,
+        exchangeHelpPointSaveGateway: ExchangeHelpPointSaveRepository,
+        saveMessageGateway: SaveMessageGateway,
+    ): ExchangeHelpPointUseCase {
+        return ExchangeHelpPointInteractor(
+            operationGateway = operationGateway,
+            exchangeHelpPointSaveGateway = exchangeHelpPointSaveGateway,
             saveMessageGateway = saveMessageGateway,
         )
     }
