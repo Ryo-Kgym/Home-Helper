f5c9c85dd15ac299cf52f36d3b4ac3c4d16647b6
Ryo-Kgym <mraytoh@icloud.com>
2022-12-27 23:51:12 +0900

    - Removed the SearchHelpPointMapper.xml - Refactored the SearchHelpPointEarnedAchievementRepository.kt

--- home-helper-api/src/main/kotlin/home/helper/api/persistence/database/mysql/help_point/search/SearchHelpPointEarnedAchievementRepository.kt
+++ home-helper-api/src/main/kotlin/home/helper/api/persistence/database/mysql/help_point/search/SearchHelpPointEarnedAchievementRepository.kt
@@ -4,7 +4,10 @@
 
 package home.helper.api.persistence.database.mysql.help_point.search
 
+import java.time.LocalDate
+import org.mybatis.dynamic.sql.select.render.SelectStatementProvider
 import org.springframework.stereotype.Repository
+import home.helper.api.persistence.database.mysql.table.DbHelpPointEarnedAchievementMapper
 import home.helper.core.domain.model.help_point.HelpPoint
 import home.helper.core.domain.model.help_point.earned.HelpPointEarnedAchievement
 import home.helper.core.domain.model.help_point.earned.HelpPointEarnedAchievementId
@@ -14,22 +17,36 @@ import home.helper.core.gateway.help_point.SearchHelpPointEarnedAchievementGatew
 
 @Repository
 class SearchHelpPointEarnedAchievementRepository(
-    private val searchHelpPointMapper: SearchHelpPointMapper,
+    private val dbHelpPointEarnedAchievementMapper: DbHelpPointEarnedAchievementMapper,
 ) : SearchHelpPointEarnedAchievementGateway {
 
     override fun search(criteria: SearchHelpPointEarnedCriteria): List<HelpPointEarnedAchievement> {
-        val param = SearchHelpPointParam(
-            userId = criteria.userId.id,
-        )
 
-        return searchHelpPointMapper.search(param)
+        return dbHelpPointEarnedAchievementMapper.selectMany(
+            generateProvider(criteria)
+        )
             .map {
                 HelpPointEarnedAchievement(
-                    achievementId = HelpPointEarnedAchievementId(it.id!!),
+                    achievementId = HelpPointEarnedAchievementId(it.id!!.toLong()),
                     userId = UserId(it.userId!!),
-                    earnedDate = it.earnedDate!!,
+                    earnedDate = LocalDate.from(it.earnedDatetime!!),
                     earnedPoint = HelpPoint(it.earnedPoint!!),
                 )
             }
     }
+
+    private fun generateProvider(criteria: SearchHelpPointEarnedCriteria) =
+        object : SelectStatementProvider {
+            override fun getParameters(): MutableMap<String, Any> {
+                return mutableMapOf(
+                    "userId" to criteria.userId.id
+                )
+            }
+
+            override fun getSelectStatement(): String {
+                return "select * from help_point_earned_achievement " +
+                        "where user_id = #{parameters.userId} " +
+                        "order by earned_datetime, id"
+            }
+        }
 }
