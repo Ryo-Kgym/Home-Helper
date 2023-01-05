3ae63364d02279a589367c791417cb7963706ebe
Ryo-Kgym <mraytoh@icloud.com>
2023-01-06 00:13:32 +0900

    - Replaced the div to Grid.

--- home-helper-front/src/components/ui/Card/LinkList.tsx
+++ home-helper-front/src/components/ui/Card/LinkList.tsx
@@ -1,21 +1,23 @@
-import styles from "@components/ui/ui.module.scss";
 import { FC } from "react";
-import { index as Card, CardProps } from "@components/ui/Card/index";
+import { CardProps, index as Card } from "@components/ui/Card/index";
+import { Grid } from "@components/ui/Grid";
 
 export const LinkList: FC<{ props: CardProps[] }> = ({ props }) => {
   return (
-    <div className={styles.grid}>
-      {props.map((p, i) => {
-        return (
-          <Card
-            href={p.href}
-            label={p.label}
-            back={p.back}
-            handleClick={p.handleClick}
-            key={"card" + i}
-          />
-        );
-      })}
-    </div>
+    <Grid>
+      <>
+        {props.map((p, i) => {
+          return (
+            <Card
+              href={p.href}
+              label={p.label}
+              back={p.back}
+              handleClick={p.handleClick}
+              key={"card" + i}
+            />
+          );
+        })}
+      </>
+    </Grid>
   );
 };
