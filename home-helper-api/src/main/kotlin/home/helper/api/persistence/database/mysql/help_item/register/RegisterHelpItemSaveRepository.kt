90974e1b725cf75c2528aa931b8f769096de0cee
Ryo-Kgym <mraytoh@icloud.com>
2023-02-06 23:38:35 +0900

    - Resolved the problem that the id is empty when the help_item is registered.

--- home-helper-api/src/main/kotlin/home/helper/api/persistence/database/mysql/help_item/register/RegisterHelpItemSaveRepository.kt
+++ home-helper-api/src/main/kotlin/home/helper/api/persistence/database/mysql/help_item/register/RegisterHelpItemSaveRepository.kt
@@ -7,10 +7,19 @@ package home.helper.api.persistence.database.mysql.help_item.register
 import home.helper.api.persistence.database.ListSaveRepository
 import home.helper.core.dto.help_item.RegisterHelpItemOutput
 import org.springframework.stereotype.Repository
+import home.helper.api.gateway.id.IdGateway
+import home.helper.core.domain.model.help_item.HelpItemId
+import home.helper.core.dto.save.SaveOutput
 
 @Repository
 class RegisterHelpItemSaveRepository(
+    private val helpItemIdGateway: IdGateway<HelpItemId>,
     helpItemInsertRepository: HelpItemInsertRepository,
 ) : ListSaveRepository<RegisterHelpItemOutput>(
     helpItemInsertRepository,
-)
+) {
+    override fun save(target: RegisterHelpItemOutput): SaveOutput {
+        helpItemIdGateway.increment()
+        return super.save(target)
+    }
+}
