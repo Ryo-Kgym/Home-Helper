8c839b96a90acde3fa160463f8e64d6df4b4b486
Ryo-Kgym <mraytoh@icloud.com>
2023-02-09 00:09:30 +0900

    - Added the function that register the exchange-item.

--- home-helper-front/src/components/container/exchange_item/register/ExchangeItemRegisterContainer.tsx
+++ home-helper-front/src/components/container/exchange_item/register/ExchangeItemRegisterContainer.tsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import { registerHelpItem } from "@hooks/help_item/registerHelpItem";
 import { showNotification } from "@hooks/notification/showNotification";
 import { ExchangeItemRegisterPresenter } from "@components/presenter/exchange_item/register/ExchangeItemRegisterPresenter";
+import { registerExchangeItem } from "@hooks/exchange_item/registerExchangeItem";
 
 export const ExchangeItemRegisterContainer = () => {
   const [itemName, setItemName] = useState<string>("");
   const [point, setPoint] = useState<number>(0);
-  const [memo, setMemo] = useState<string>("");
 
   const register = () => {
     if (itemName.length < 1) {
       return;
     }
-    registerHelpItem(itemName, point, memo).then((r) => {
+    registerExchangeItem(itemName, point).then((r) => {
       showNotification({
         title: "登録完了",
         message: r[0],
@@ -26,8 +25,6 @@ export const ExchangeItemRegisterContainer = () => {
       setItemName={setItemName}
       point={point}
       setPoint={setPoint}
-      memo={memo}
-      setMemo={setMemo}
       registerOnClick={register}
     />
   );
--- home-helper-front/src/components/presenter/exchange_item/register/ExchangeItemRegisterPresenter.tsx
+++ home-helper-front/src/components/presenter/exchange_item/register/ExchangeItemRegisterPresenter.tsx
@@ -8,25 +8,14 @@ type ExchangeItemRegisterPresenterProps = {
   setItemName: (value: string) => void;
   point: number;
   setPoint: (value: number) => void;
-  memo: string;
-  setMemo: (value: string) => void;
   registerOnClick: () => void;
 };
 export const ExchangeItemRegisterPresenter: FC<
   ExchangeItemRegisterPresenterProps
-> = ({
-  itemName,
-  setItemName,
-  point,
-  setPoint,
-  memo,
-  setMemo,
-  registerOnClick,
-}) => (
+> = ({ itemName, setItemName, point, setPoint, registerOnClick }) => (
   <div className={"text-4xl"}>
     <ItemNameItem value={itemName} setValue={setItemName} />
     <PointItem value={point} setValue={setPoint} />
-    <MemoItem value={memo} setValue={setMemo} />
     <div className={"grid place-content-center"}>
       <Button label={"登録"} onClick={registerOnClick} />
     </div>
@@ -61,18 +50,6 @@ const PointItem = ({ value, setValue }: ItemProps) => (
     />
   </ItemStyler>
 );
-
-const MemoItem = ({ value, setValue }: ItemProps) => (
-  <ItemStyler>
-    <Input
-      label={"メモ"}
-      value={value}
-      onChange={setValue}
-      placeholder={"自由入力です。"}
-    />
-  </ItemStyler>
-);
-
 type ItemProps = {
   value: string | number;
   setValue: (value: any) => void;
