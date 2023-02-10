f203eaf5a5e185aad17a18561e739fbc27a3b4f3
Ryo-Kgym <mraytoh@icloud.com>
2023-02-11 01:25:21 +0900

    Used the split.

--- home-helper-front/src/components/page/account/AccountPage.tsx
+++ home-helper-front/src/components/page/account/AccountPage.tsx
@@ -1,11 +1,12 @@
 import { HouseHoldLayout } from "@components/atoms/Layout/HouseHoldLayout";
 import { AccountContainer } from "@components/organisms/account/AccountContainer";
 import { FC } from "react";
+import { Split } from "@components/atoms/Split";
 
 export const Page: FC = () => {
   return (
     <HouseHoldLayout>
-      <AccountContainer />
+      <Split first={<AccountContainer />} second={<AccountContainer />} />
     </HouseHoldLayout>
   );
 };
