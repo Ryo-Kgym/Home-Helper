dbc5ebc5d4ea6d1f067b34de7d35b1f6e8519bec
Ryo-Kgym <mraytoh@icloud.com>
2023-02-18 14:13:44 +0900

    Changed the setting to the simple-collections

--- home-helper-database/postgraphile_boot.sh
+++ home-helper-database/postgraphile_boot.sh
@@ -3,4 +3,5 @@ DATABASE_URL="postgres://household:household@localhost:5432/household_db"
 postgraphile \
   --export-schema-graphql household.graphql \
   --connection $DATABASE_URL \
-  --schema public
+  --schema public \
+  --simple-collections only
