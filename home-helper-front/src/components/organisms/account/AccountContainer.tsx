34e9ccf4429cdd1f960e6396ce0fa6a66c569d47
Ryo-Kgym <mraytoh@icloud.com>
2023-02-20 13:17:42 +0900

    Fixed to use the useGetAccountsQuery.

--- home-helper-front/src/components/organisms/account/AccountContainer.tsx
+++ home-helper-front/src/components/organisms/account/AccountContainer.tsx
@@ -1,24 +1,28 @@
 import { AccountPresenter } from "@components/organisms/account/AccountPresenter";
 import { TbodyProps } from "@components/atoms/Table";
-import { Account } from "@domain/model/household/Account";
+import { useGetAccountsQuery } from "@graphql/postgraphile/generated/graphql";
 
 export const AccountContainer = () => {
-  const tbodyProps: TbodyProps[] = mockAccount.map((account) => {
-    return {
-      keyPrefix: "account",
-      columns: [
-        { value: account.accountName, align: "left" },
-        { value: account.balance.toLocaleString(), align: "right" },
-      ],
-    };
-  });
+  const [{ data, fetching, error }] = useGetAccountsQuery();
+  // TODO loading, error の処理を別コンポに委譲
+  if (fetching) return <div>loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  const tbodyProps: TbodyProps[] =
+    data?.allMAccountsList?.map((account) => {
+      return {
+        keyPrefix: "account",
+        columns: [
+          { value: account.accountName, align: "left" },
+          {
+            value: Number(
+              account.accountBalanceByAccountId?.balance
+            ).toLocaleString(),
+            align: "right",
+          },
+        ],
+      };
+    }) ?? [];
 
   return <AccountPresenter tbodyProps={tbodyProps} />;
 };
-
-const mockAccount: Account[] = [
-  { accountId: "1", accountName: "財布1", balance: 10000 },
-  { accountId: "2", accountName: "財布2", balance: 5000 },
-  { accountId: "3", accountName: "A銀行", balance: 100000 },
-  { accountId: "4", accountName: "B銀行", balance: 200000 },
-];
--- home-helper-front/src/pages/_app.tsx
+++ home-helper-front/src/pages/_app.tsx
@@ -2,13 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NotificationsProvider } from "@mantine/notifications";
 import { MantineProvider } from "@mantine/core";
+import { client } from "@graphql/postgraphile/client";
+import { Provider } from "urql";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withNormalizeCSS withGlobalStyles>
-      <NotificationsProvider>
-        <Component {...pageProps} />
-      </NotificationsProvider>
-    </MantineProvider>
+    <Provider value={client}>
+      <MantineProvider withNormalizeCSS withGlobalStyles>
+        <NotificationsProvider>
+          <Component {...pageProps} />
+        </NotificationsProvider>
+      </MantineProvider>
+    </Provider>
   );
 }
