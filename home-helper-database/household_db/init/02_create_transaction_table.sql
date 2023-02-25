4ad8e681d363ccfea919f7379ebb58f4a9d93778
Ryo-Kgym <mraytoh@icloud.com>
2023-02-25 09:33:35 +0900

    Renamed the master table.

--- home-helper-database/household_db/init/02_create_transaction_table.sql
+++ home-helper-database/household_db/init/02_create_transaction_table.sql
@@ -2,7 +2,7 @@ drop table if exists account_balance cascade;
 create table account_balance (
     account_id varchar(8) not null primary key,
     balance    numeric(10) default 0,
-    constraint account_balance_account foreign key (account_id) references m_account (account_id)
+    constraint account_balance_account foreign key (account_id) references account (account_id)
 );
 
 drop table if exists daily_detail cascade;
@@ -14,9 +14,9 @@ create table daily_detail (
     user_id     varchar(8)  not null,
     amount      numeric(10) not null,
     memo        varchar(64) null,
-    constraint daily_detail_category foreign key (category_id) references m_category (category_id),
-    constraint daily_detail_account foreign key (account_id) references m_account (account_id),
-    constraint daily_detail_user foreign key (user_id) references m_user (user_id)
+    constraint daily_detail_category foreign key (category_id) references "category" (category_id),
+    constraint daily_detail_account foreign key (account_id) references account (account_id),
+    constraint daily_detail_user foreign key (user_id) references "user" (user_id)
 );
 
 create index daily_detail_date_index on daily_detail (date);
