bc875d2abc61df4e17334e95b0b0179f71832626
Ryo-Kgym <mraytoh@icloud.com>
2023-02-27 00:51:12 +0900

    Created the today background-color. Changed the background-color.

--- home-helper-front/src/components/organisms/calendar/day/DayContainer.tsx
+++ home-helper-front/src/components/organisms/calendar/day/DayContainer.tsx
@@ -6,6 +6,8 @@ type DayContainerProps = {
   date: Date;
 };
 export const DayContainer: FC<DayContainerProps> = ({ date }) => {
+  const today = new Date();
+
   const displayDate =
     date.getDate() === 1
       ? date.getMonth() + 1 + "/" + date.getDate()
@@ -13,7 +15,16 @@ export const DayContainer: FC<DayContainerProps> = ({ date }) => {
 
   const { income, outcome } = useGetAmountByDay(date);
 
+  const isToday = date.toDateString() === today.toDateString();
+  const isNotThisMonth = date.getMonth() !== today.getMonth();
+
   return (
-    <DayPresenter displayDay={displayDate} income={income} outcome={outcome} />
+    <DayPresenter
+      displayDay={displayDate}
+      income={income}
+      outcome={outcome}
+      isToday={isToday}
+      isNotThisMonth={isNotThisMonth}
+    />
   );
 };
--- home-helper-front/src/components/organisms/calendar/day/DayPresenter.tsx
+++ home-helper-front/src/components/organisms/calendar/day/DayPresenter.tsx
@@ -4,21 +4,28 @@ type DayPresenterProps = {
   displayDay: string;
   income: number | undefined;
   outcome: number | undefined;
+  isToday: boolean;
+  isNotThisMonth: boolean;
 };
 export const DayPresenter: FC<DayPresenterProps> = ({
   displayDay,
   income,
   outcome,
+  isToday,
+  isNotThisMonth,
 }) => {
   return (
-    <div className={"h-32 border-r-2 border-b-2"}>
+    <div
+      className={`h-32 border-r-2 border-b-2 
+      ${isToday && "bg-amber-100"} ${isNotThisMonth && "bg-gray-300"}`}
+    >
       <div className={"p-2"}>{displayDay}</div>
 
       <div className={"grid grid-cols-1"}>
-        {income && <Line price={income} backgroundColor={"bg-green-500"} />}
-        {outcome && <Line price={outcome} backgroundColor={"bg-red-500"} />}
+        <Line price={income} backgroundColor={`${income && "bg-green-300"} `} />
+        <Line price={outcome} backgroundColor={`${outcome && "bg-red-300"} `} />
         {income && outcome && (
-          <Line price={income - outcome} backgroundColor={"bg-yellow-500"} />
+          <Line price={income - outcome} backgroundColor={"bg-yellow-300"} />
         )}
       </div>
     </div>
@@ -27,14 +34,16 @@ export const DayPresenter: FC<DayPresenterProps> = ({
 
 const Line = ({
   price,
-  backgroundColor,
+  backgroundColor = "bg-inherit",
 }: {
-  price: number;
-  backgroundColor: string;
+  price?: number;
+  backgroundColor?: string;
 }) => {
   return (
-    <div className={`m-0.5 pr-1 rounded-md ${backgroundColor} text-right`}>
-      {price.toLocaleString()}
+    <div
+      className={`m-0.5 pr-1 rounded-md ${backgroundColor} text-right h-[1.5em]`}
+    >
+      {price?.toLocaleString() ?? ""}
     </div>
   );
 };
