f58d00aca77c338af8c081da388a8727430f55ff
Ryo-Kgym <mraytoh@icloud.com>
2023-03-01 23:27:53 +0900

    Added the properties on RegisterDailyDetail

--- home-helper-front/src/components/organisms/register_daily_detail/RegisterDailyDetailContainer.tsx
+++ home-helper-front/src/components/organisms/register_daily_detail/RegisterDailyDetailContainer.tsx
@@ -1,9 +1,36 @@
-import { FC } from "react";
-import { RegisterDailyDetailPresenter } from "@components/organisms/register_daily_detail/RegisterDailyDetailPresenter";
+import { FC, useState } from "react";
+import { RegisterDailyDetailPresenter } from "./RegisterDailyDetailPresenter";
 
-type RegisterDailyDetailContainerProps = {};
+type RegisterDailyDetailContainerProps = {
+  date: Date;
+};
 export const RegisterDailyDetailContainer: FC<
   RegisterDailyDetailContainerProps
-> = () => {
-  return <RegisterDailyDetailPresenter />;
+> = ({ date }) => {
+  const [iocomeType, setIocomeType] = useState<"INCOME" | "OUTCOME">("INCOME");
+  const [categoryId, setCategoryId] = useState("");
+  const [genreId, setGenreId] = useState("");
+  const [accountId, setAccountId] = useState("");
+  const [amount, setAmount] = useState("");
+  const [memo, setMemo] = useState("");
+
+  const displayDate = date.toLocaleDateString();
+
+  return (
+    <RegisterDailyDetailPresenter
+      displayDate={displayDate}
+      iocomeType={iocomeType}
+      setIocomeType={setIocomeType}
+      categoryId={categoryId}
+      setCategoryId={setCategoryId}
+      genreId={genreId}
+      setGenreId={setGenreId}
+      accountId={accountId}
+      setAccountId={setAccountId}
+      amount={amount}
+      setAmount={setAmount}
+      memo={memo}
+      setMemo={setMemo}
+    />
+  );
 };
--- home-helper-front/src/components/organisms/register_daily_detail/RegisterDailyDetailPresenter.tsx
+++ home-helper-front/src/components/organisms/register_daily_detail/RegisterDailyDetailPresenter.tsx
@@ -1,22 +1,66 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
+import { GenreSelect } from "@components/molecules/CustomSelect/Genre";
+import { CategorySelect } from "@components/molecules/CustomSelect/Category";
+import { AccountSelect } from "@components/molecules/CustomSelect/Account";
 
-type RegisterDailyDetailPresenterProps = {};
+type RegisterDailyDetailPresenterProps = {
+  displayDate: string;
+  iocomeType: "INCOME" | "OUTCOME";
+  setIocomeType: (value: "INCOME" | "OUTCOME") => void;
+  categoryId: string;
+  setCategoryId: (value: string) => void;
+  genreId: string;
+  setGenreId: (value: string) => void;
+  accountId: string;
+  setAccountId: (value: string) => void;
+  amount: string;
+  setAmount: (value: string) => void;
+  memo: string;
+  setMemo: (value: string) => void;
+};
 export const RegisterDailyDetailPresenter: FC<
   RegisterDailyDetailPresenterProps
-> = () => (
-  <div className={"grid grid-cols-2 w-full"}>
-    <div>日付変更</div>
-    <div>区分</div>
-    <div>コンポーネント</div>
-    <div>ジャンル</div>
-    <div>コンポーネント</div>
-    <div>カテゴリ</div>
-    <div>コンポーネント</div>
-    <div>アカウント</div>
-    <div>コンポーネント</div>
-    <div>金額</div>
-    <div>コンポーネント</div>
-    <div>メモ</div>
-    <div>コンポーネント</div>
+> = ({
+  displayDate,
+  iocomeType,
+  setIocomeType,
+  categoryId,
+  setCategoryId,
+  genreId,
+  setGenreId,
+  accountId,
+  setAccountId,
+  amount,
+  setAmount,
+  memo,
+  setMemo,
+}) => (
+  <div className={"grid grid-cols-2 w-full text-3xl"}>
+    <div className={"col-span-2 text-center"}>{displayDate}</div>
+    <Field label={"区分"}>
+      <GenreSelect iocomeType={"INCOME"} />
+    </Field>
+    <Field label={"ジャンル"}>
+      <GenreSelect iocomeType={"OUTCOME"} />
+    </Field>
+    <Field label={"カテゴリ"}>
+      <CategorySelect genreId={"GNR00001"} />
+    </Field>
+    <Field label={"アカウント"}>
+      <AccountSelect />
+    </Field>
+    <Field label={"金額"}>
+      <input type={"number"} />
+    </Field>
+    <Field label={"メモ"}>
+      <input type={"text"} />
+    </Field>
   </div>
+);
+
+const Field = ({ label, children }: { label: string; children: ReactNode }) => (
+  <>
+    <div>{label}</div>
+    <div>{children}</div>
+  </>
 );
