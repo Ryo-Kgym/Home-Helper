9fdc2854367ae95481c62d987e892746b778e7e4
Ryo-Kgym <mraytoh@icloud.com>
2023-03-08 22:28:04 +0900

    Added the Calendar page on category.

--- home-helper-front/src/components/page/category/CategoryPage.tsx
+++ home-helper-front/src/components/page/category/CategoryPage.tsx
@@ -2,11 +2,12 @@ import { HouseHoldLayout } from "@components/atoms/Layout/HouseHoldLayout";
 import { FC } from "react";
 import { Split } from "@components/atoms/Split";
 import { CategoryContainer } from "@components/organisms/category/CategoryContainer";
+import { CalendarContainer } from "@components/organisms/calendar/CalendarContainer";
 
 export const Page: FC = () => {
   return (
     <HouseHoldLayout>
-      <Split first={<CategoryContainer />} second={<CategoryContainer />} />
+      <Split first={<CalendarContainer />} second={<CategoryContainer />} />
     </HouseHoldLayout>
   );
 };
