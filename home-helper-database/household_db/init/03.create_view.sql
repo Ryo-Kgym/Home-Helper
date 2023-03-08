c667a6da7894c983b22a834d62ae9c0429e27afb
Ryo-Kgym <mraytoh@icloud.com>
2023-03-08 23:23:48 +0900

    Created the total_by_account_view.

--- home-helper-database/household_db/init/03.create_view.sql
+++ home-helper-database/household_db/init/03.create_view.sql
@@ -13,3 +13,31 @@ from
 group by
     d.date,
     g.iocome_type;
+
+drop view if exists total_by_account_view cascade;
+create view total_by_account_view as
+select
+    d.account_id,
+    a.account_name,
+    a.display_order,
+    g.iocome_type,
+    sum(d.amount) as total
+from
+    daily_detail d
+    inner join account a
+        on a.account_id = d.account_id
+    inner join category c
+        on d.category_id = c.category_id
+    inner join genre g
+        on c.genre_id = g.genre_id
+group by
+    d.account_id,
+    a.account_name,
+    a.display_order,
+    g.iocome_type
+order by
+    a.display_order,
+    d.account_id,
+    a.account_name,
+    g.iocome_type;
+;
--- home-helper-database/household_db/init/04.create_function.sql
+++ home-helper-database/household_db/init/04.create_function.sql
@@ -1,9 +1,20 @@
 drop function if exists daily_total_view_by_date_iocome_type cascade;
-create function daily_total_view_by_date_iocome_type(iocome_type iocome_type, date date) returns setof daily_total_view as
+create function daily_total_view_by_date_iocome_type(iocome_type iocome_type, date date)
+    returns setof daily_total_view as
 $$
 select *
 from
     daily_total_view d
 where d.date = daily_total_view_by_date_iocome_type.date
   and d.iocome_type = daily_total_view_by_date_iocome_type.iocome_type;
+$$ language sql stable;
+
+drop function if exists total_by_account_id cascade;
+create function total_by_account_id(account_id text)
+    returns setof total_by_account_view as
+$$
+select *
+from
+    total_by_account_view t
+where t.account_id = total_by_account_id.account_id;
 $$ language sql stable;
