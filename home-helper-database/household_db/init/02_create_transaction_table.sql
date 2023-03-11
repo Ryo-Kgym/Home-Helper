40fb74b63a13ba57a0b83850d70fdeb399679e00
Ryo-Kgym <mraytoh@icloud.com>
2023-03-11 21:56:59 +0900

    Removed the account_balance.

--- home-helper-database/household_db/init/02_create_transaction_table.sql
+++ home-helper-database/household_db/init/02_create_transaction_table.sql
@@ -1,10 +1,3 @@
-drop table if exists account_balance cascade;
-create table account_balance (
-    account_id varchar(8) not null primary key,
-    balance    numeric(10) default 0,
-    constraint account_balance_account foreign key (account_id) references account (account_id)
-);
-
 drop table if exists daily_detail cascade;
 create table daily_detail (
     serial_no   serial      not null primary key,
