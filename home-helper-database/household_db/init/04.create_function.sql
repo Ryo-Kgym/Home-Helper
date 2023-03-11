beb23afb496eece90980e391633bfb21fd24fbb7
Ryo-Kgym <mraytoh@icloud.com>
2023-03-11 22:00:52 +0900

    Removed the total_by_account_id.

--- home-helper-database/household_db/init/04.create_function.sql
+++ home-helper-database/household_db/init/04.create_function.sql
@@ -21,16 +21,6 @@ where d.date = daily_total_view_by_date_iocome_type.date
   and d.iocome_type = daily_total_view_by_date_iocome_type.iocome_type;
 $$ language sql stable;
 
-drop function if exists total_by_account_id cascade;
-create function total_by_account_id(account_id text)
-    returns setof total_by_account_view as
-$$
-select *
-from
-    total_by_account_view t
-where t.account_id = total_by_account_id.account_id;
-$$ language sql stable;
-
 drop function if exists category_total_by_month cascade;
 create function category_total_by_month(from_date date, to_date date)
     returns setof total_by_category_view as
