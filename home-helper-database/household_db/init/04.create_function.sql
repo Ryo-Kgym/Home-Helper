fe4936bf361be7bfb1669e8d3eed168e8535866c
Ryo-Kgym <mraytoh@icloud.com>
2023-03-11 22:32:35 +0900

    Change the account_total.

--- home-helper-database/household_db/init/04.create_function.sql
+++ home-helper-database/household_db/init/04.create_function.sql
@@ -43,3 +43,22 @@ group by
 order by
     t.total desc;
 $$ language sql stable;
+
+drop function if exists account_total cascade;
+create function account_total() returns setof total_by_account_view as
+$$
+select
+    account_id,
+    account_name,
+    display_order,
+    'INCOME'::iocome_type as iocome_type,
+    sum(case iocome_type when 'INCOME' then 1 else -1 end * total) as total
+from
+    total_by_account_view
+group by
+    account_id,
+    account_name,
+    display_order
+order by
+    display_order desc;
+$$ language sql stable;
