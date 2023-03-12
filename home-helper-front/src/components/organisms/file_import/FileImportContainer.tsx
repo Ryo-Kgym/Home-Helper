5609a283fe81f736098dec112fad49990e233bf9
Ryo-Kgym <mraytoh@icloud.com>
2023-03-13 00:31:22 +0900

    Added the state. Changed the layout.

--- home-helper-front/src/components/organisms/file_import/FileImportContainer.tsx
+++ home-helper-front/src/components/organisms/file_import/FileImportContainer.tsx
@@ -1,8 +1,13 @@
 import { FileImportPresenter } from "@components/organisms/file_import/FileImportPresenter";
 import { TableProps } from "@components/atoms/Table";
 import category from "@pages/household/category";
+import { useState } from "react";
 
 export const FileImportContainer = () => {
+  const [uploadFile, setUploadFile] = useState<File | null>(null);
+  const [accountId, setAccountId] = useState<string | null>(null);
+  const [withdrawalDate, setWithdrawalDate] = useState<Date | null>(null);
+
   const tableProps: TableProps[] = mockImported.map((d) => {
     return {
       keyPrefix: "category",
@@ -17,16 +22,17 @@ export const FileImportContainer = () => {
   });
 
   return (
-    <FileImportPresenter tableProps={tableProps} yearMonths={mockYearMonth} />
+    <FileImportPresenter
+      uploadFile={uploadFile}
+      setUploadFile={setUploadFile}
+      accountId={accountId}
+      setAccountId={setAccountId}
+      tableProps={tableProps}
+      withdrawalDate={withdrawalDate}
+      changeWithdrawalDate={setWithdrawalDate}
+    />
   );
 };
-
-const mockYearMonth = [
-  { yearMonth: "2023-01" },
-  { yearMonth: "2023-02" },
-  { yearMonth: "2023-03" },
-  { yearMonth: "2023-04" },
-];
 
 const mockImported = [
   { date: "2023-02-01", note: "Amazon Payで", price: 12000 },
--- home-helper-front/src/components/organisms/file_import/FileImportPresenter.tsx
+++ home-helper-front/src/components/organisms/file_import/FileImportPresenter.tsx
@@ -1,30 +1,48 @@
 import { FC } from "react";
 import { Table, TableProps } from "@components/atoms/Table";
-import { GridTable } from "@components/molecules/GridTable";
+import { FileInput } from "@components/atoms/FileInput";
+import { AccountSelect } from "@components/molecules/CustomSelect/Account";
+import { DatePicker } from "@components/atoms/DatePicker";
 
 type FileImportPresenterProps = {
+  uploadFile: File | null;
+  setUploadFile: (file: File | null) => void;
+  accountId: string | null;
+  setAccountId: (accountId: string | null) => void;
+  withdrawalDate: Date | null;
+  changeWithdrawalDate: (date: Date | null) => void;
   tableProps: TableProps[];
-  yearMonths: any[];
 };
 export const FileImportPresenter: FC<FileImportPresenterProps> = ({
-  yearMonths,
+  uploadFile,
+  setUploadFile,
+  accountId,
+  setAccountId,
+  withdrawalDate,
+  changeWithdrawalDate,
   tableProps,
-}) => {
-  return (
-    <>
-      <GridTable
-        gridProps={[
-          { label: "ファイル選択", components: "フィールド" },
-          { label: "適用年月", components: "フィールド" },
-          { label: "アカウント", components: "フィールド" },
-        ]}
+}) => (
+  <div className={"grid"}>
+    <div className={"py-4"}>
+      <FileInput file={uploadFile} setFile={setUploadFile} />
+    </div>
+    <div className={"py-4"}>
+      <AccountSelect accountId={accountId} setAccountId={setAccountId} />
+    </div>
+    <div className={"py-4"}>
+      <DatePicker
+        value={withdrawalDate!}
+        onChange={changeWithdrawalDate}
+        required
+        label={"WITHDRAWAL DATE"}
       />
-      <div>
-        <Table
-          header={["決済日", "備考", "支出金額", "ジャンル", "カテゴリ"]}
-          tablePropsList={tableProps}
-        />
-      </div>
-    </>
-  );
-};
+    </div>
+    <div>
+      <Table
+        header={["決済日", "備考", "支出金額", "ジャンル", "カテゴリ"]}
+        tablePropsList={tableProps}
+        size={"xs"}
+      />
+    </div>
+  </div>
+);
