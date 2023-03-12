fd1e43751aa51c8deffee8e73aff1d87c8d29322
Ryo-Kgym <mraytoh@icloud.com>
2023-03-12 14:19:47 +0900

    Added the label property.

--- home-helper-front/src/components/atoms/Button/MonthPicker/MonthPickerPresenter.tsx
+++ home-helper-front/src/components/atoms/Button/MonthPicker/MonthPickerPresenter.tsx
@@ -4,14 +4,16 @@ import { MonthPickerInput } from "@mantine/dates";
 type MonthPickerPresenterProps = {
   value: Date | null;
   changeValue: (value: Date | null) => void;
+  label?: string;
 };
 export const MonthPickerPresenter: FC<MonthPickerPresenterProps> = ({
   value,
   changeValue,
+  label = "YEAR MONTH",
 }) => {
   return (
     <MonthPickerInput
-      label="YEAR MONTH"
+      label={label}
       placeholder="YYYY-MM"
       valueFormat={"YYYY-MM"}
       yearLabelFormat={"YYYY"}
