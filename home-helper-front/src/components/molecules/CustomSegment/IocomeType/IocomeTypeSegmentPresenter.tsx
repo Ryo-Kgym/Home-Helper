75f03d6681e75c70ad496734acfa7fe6a8376c3a
Ryo-Kgym <mraytoh@icloud.com>
2023-03-19 02:07:55 +0900

    Added the disabled props.

--- home-helper-front/src/components/molecules/CustomSegment/IocomeType/IocomeTypeSegmentPresenter.tsx
+++ home-helper-front/src/components/molecules/CustomSegment/IocomeType/IocomeTypeSegmentPresenter.tsx
@@ -5,12 +5,13 @@ import { IocomeType } from "@domain/model/household/IocomeType";
 type IocomeTypeSegmentPresenterProps = {
   value: string;
   onChange: (value: string) => void;
+  disabled: boolean;
 };
 
 export const IocomeTypeSegmentPresenter: FC<
   IocomeTypeSegmentPresenterProps
-> = ({ value, onChange }) => (
-  <Segment value={value} onChange={onChange} data={data} />
+> = ({ value, onChange, disabled }) => (
+  <Segment value={value} onChange={onChange} data={data} disabled={disabled} />
 );
 
 const data = [
