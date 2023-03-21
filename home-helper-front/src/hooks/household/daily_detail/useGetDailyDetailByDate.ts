c0062e2f97d63982c9127f1ebc090fc0c1148beb
Ryo-Kgym <mraytoh@icloud.com>
2023-03-22 01:48:57 +0900

    Added the getDailyDetail func.

--- home-helper-front/src/hooks/household/daily_detail/useGetDailyDetailByDate.ts
+++ home-helper-front/src/hooks/household/daily_detail/useGetDailyDetailByDate.ts
@@ -28,5 +28,40 @@ export const useGetDailyDetailByDate = (
     )
     .reduce((a, b) => a + Number(b!.amount!), 0);
 
-  return { data, fetching, error, incomeTotal, outcomeTotal };
+  const getDailyDetail = (serialNo: number): DailyDetail => {
+    const dailyDetail = data?.dailyDetailByDateList?.find(
+      (e) => e!.serialNo === serialNo
+    );
+
+    return {
+      date: new Date(dailyDetail?.date),
+      amount: Number(dailyDetail?.amount) ?? "",
+      iocomeType:
+        dailyDetail?.categoryByCategoryId?.genreByGenreId?.iocomeType ?? null,
+      genreId:
+        dailyDetail?.categoryByCategoryId?.genreByGenreId?.genreId ?? null,
+      categoryId: dailyDetail?.categoryByCategoryId?.categoryId ?? null,
+      accountId: dailyDetail?.accountByAccountId?.accountId ?? null,
+      memo: dailyDetail?.memo ?? null,
+    };
+  };
+
+  return {
+    data,
+    fetching,
+    error,
+    incomeTotal,
+    outcomeTotal,
+    getDetail: getDailyDetail,
+  };
+};
+
+export type DailyDetail = {
+  date: Date | null;
+  amount: number | "";
+  iocomeType: IocomeType | null;
+  genreId: string | null;
+  categoryId: string | null;
+  accountId: string | null;
+  memo: string | null;
 };
