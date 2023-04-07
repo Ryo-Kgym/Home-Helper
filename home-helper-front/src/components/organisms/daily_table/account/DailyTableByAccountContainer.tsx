6385548f06de10f5fec71293035d445e1146282c
Ryo-Kgym <mraytoh@icloud.com>
2023-04-07 22:49:25 +0900

    Called the credit query.

--- home-helper-front/src/components/organisms/daily_table/account/DailyTableByAccountContainer.tsx
+++ home-helper-front/src/components/organisms/daily_table/account/DailyTableByAccountContainer.tsx
@@ -3,6 +3,8 @@ import { TableProps } from "@components/atoms/Table";
 import { DailyTableByAccountPresenter } from "./DailyTableByAccountPresenter";
 import { dailyDetailConverter } from "@components/organisms/daily_table/dailyDetailConverter";
 import { useGetDailyDetailByDateAccountId } from "@hooks/household/daily_detail/useGetDailyDetailByDateAccountId";
+import { useGetCreditCardSummaryByAccountIdBetweenDate } from "@hooks/household/credit_card/useGetCreditCardSummaryByAccountIdBetweenDate";
+import { creditCardSummaryConverter } from "@components/organisms/daily_table/creditCardSummaryConverter";
 
 type DailyTableByAccountContainerProps = {
   fromDate: Date;
@@ -18,15 +20,35 @@ export const DailyTableByAccountContainer: FC<
     toDate
   );
 
-  const tableProps: TableProps[] = dailyDetailConverter({ data });
+  const {
+    data: creditCardSummaryData,
+    incomeTotal: creditCardIncomeTotal,
+    outcomeTotal: creditCardOutcomeTotal,
+  } = useGetCreditCardSummaryByAccountIdBetweenDate(
+    accountId,
+    fromDate,
+    toDate
+  );
+
+  const tableProps: TableProps[] = dailyDetailConverter({ data })
+    .concat(creditCardSummaryConverter({ data: creditCardSummaryData }))
+    .sort((a, b) => {
+      if (a.columns[0].value > b.columns[0].value) {
+        return 1;
+      } else if (a.columns[0].value < b.columns[0].value) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
 
   return (
     <DailyTableByAccountPresenter
       firstDay={fromDate}
       lastDay={toDate}
       tableProps={tableProps}
-      incomeTotal={incomeTotal}
-      outcomeTotal={outcomeTotal}
+      incomeTotal={(incomeTotal ?? 0) + creditCardIncomeTotal}
+      outcomeTotal={(outcomeTotal ?? 0) + (creditCardOutcomeTotal ?? 0)}
     />
   );
 };
