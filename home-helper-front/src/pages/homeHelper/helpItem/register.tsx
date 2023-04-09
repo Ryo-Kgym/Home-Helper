38144bfb348f0b937b8f31cab84a59f015ab5ecc
Ryo-Kgym <mraytoh@icloud.com>
2023-04-09 20:42:51 +0900

    Deleted the homeHelper page.

--- home-helper-front/src/pages/homeHelper/helpItem/register.tsx
+++
@@ -1,7 +0,0 @@
-import { Page } from "@components/page/help_item/HelpItemRegisterPage";
-import { NextPage } from "next";
-
-const index: NextPage = () => {
-  return <Page />;
-};
-export default index;
--- home-helper-front/src/pages/homeHelper/possessionPoint.tsx
+++
@@ -1,6 +0,0 @@
-import { Page } from "@components/page/prosession_point/PossessionPointPage";
-import { NextPage } from "next";
-
-const index: NextPage = () => <Page />;
-
-export default index;
