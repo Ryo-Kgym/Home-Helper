96e045d36763998d0b76997502c56062a6c7b05d
Ryo-Kgym <mraytoh@icloud.com>
2023-04-18 21:28:35 +0900

    Summaried to the userState.

--- home-helper-front/src/hooks/user/useUser.ts
+++ home-helper-front/src/hooks/user/useUser.ts
@@ -1,14 +1,11 @@
 import { useRecoilState } from "recoil";
-import { userIdState } from "@recoil/userIdState";
-import { userNameState } from "@recoil/userNameState";
+import { userState } from "@recoil/userState";
 
 export const useUser = () => {
-  const [userId, setUserId] = useRecoilState(userIdState);
-  const [userName, setUserName] = useRecoilState(userNameState);
+  const [user, setUser] = useRecoilState(userState);
 
   const save = (user: { userId: string; userName: string }) => {
-    setUserId(user.userId);
-    setUserName(user.userName);
+    setUser(user);
   };
-  return { userId, userName, save };
+  return { userId: user.userId, userName: user.userName, save };
 };
