e7af9aec7b3aaf707c99a995f3b0c90143b69332
Ryo-Kgym <mraytoh@icloud.com>
2023-04-20 01:36:06 +0900

    Created the ConfirmPrompt component.

---
+++ home-helper-front/src/components/molecules/ConfirmPrompt/ConfirmPromptContainer.tsx
@@ -0,0 +1,28 @@
+import { FC, ReactNode } from "react";
+import { ConfirmPromptPresenter } from "./ConfirmPromptPresenter";
+
+type ConfirmPromptContainerProps = {
+  title: string;
+  children: ReactNode;
+  opened: boolean;
+  onClose: () => void;
+  doneOnClickHandler: () => void;
+};
+
+export const ConfirmPromptContainer: FC<ConfirmPromptContainerProps> = ({
+  title,
+  children,
+  opened,
+  onClose,
+  doneOnClickHandler,
+}) => {
+  return (
+    <ConfirmPromptPresenter
+      title={title}
+      children={children}
+      opened={opened}
+      onClose={onClose}
+      doneOnClickHandler={doneOnClickHandler}
+    />
+  );
+};
