7ab3875332d7d6ba15696b2c8ba723685c6e9d27
Ryo-Kgym <mraytoh@icloud.com>
2023-04-20 12:10:57 +0900

    Resolved the import error.

--- home-helper-front/src/components/organisms/master/summary_category/SummaryCategoryTransferPresenter.tsx
+++ home-helper-front/src/components/organisms/master/summary_category/SummaryCategoryTransferPresenter.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { TransferList, TransferListItem } from "@components/atoms/TransferList";
 import { Button } from "@components/atoms/Button";
-import { RegisterSummaryCategoryConfirmPrompt } from "@components/organisms/master/yearlySummaryCategory/RegisterSummaryCategoryConfirmPrompt";
+import { RegisterSummaryCategoryConfirmPrompt } from "./RegisterSummaryCategoryConfirmPrompt";
 
 type SalaryTransferPresenterProps = {
   transferData: [TransferListItem[], TransferListItem[]];
