e595e39764ce48998651c2fbee9ccaf878be75cf
Ryo-Kgym <mraytoh@icloud.com>
2023-04-21 21:16:44 +0900

    Added the owner_user_id.

--- home-helper-database/household_db/init/01_create_master_table.sql
+++ home-helper-database/household_db/init/01_create_master_table.sql
@@ -18,8 +18,11 @@ CREATE TABLE genre (
     genre_type    genre_type  NOT NULL,
     iocome_type   iocome_type NOT NULL,
     valid_flag    BOOLEAN DEFAULT TRUE,
-    display_order INT         NOT NULL
+    display_order INT         NOT NULL,
+    owner_user_id uuid        NOT NULL,
+    CONSTRAINT genre_owner_user_id_fk FOREIGN KEY (owner_user_id) REFERENCES "user" (user_id)
 );
+
 
 DROP TABLE IF EXISTS "category" CASCADE;
 CREATE TABLE "category" (
@@ -28,7 +31,9 @@ CREATE TABLE "category" (
     genre_id      uuid        NOT NULL,
     valid_flag    BOOLEAN DEFAULT TRUE,
     display_order INT         NOT NULL,
-    CONSTRAINT category_genre_id_fk FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
+    owner_user_id uuid        NOT NULL,
+    CONSTRAINT category_genre_id_fk FOREIGN KEY (genre_id) REFERENCES genre (genre_id),
+    CONSTRAINT category_owner_user_id_fk FOREIGN KEY (owner_user_id) REFERENCES "user" (user_id)
 );
 
 DROP TABLE IF EXISTS account CASCADE;
@@ -44,10 +49,10 @@ CREATE TABLE account (
 DROP TABLE IF EXISTS summary_category CASCADE;
 CREATE TABLE summary_category (
     id            uuid NOT NULL PRIMARY KEY,
-    user_id       uuid NOT NULL,
     category_id   uuid NOT NULL,
     display_order INT  NOT NULL,
-    CONSTRAINT yearly_summary_category_user_id_fk FOREIGN KEY (user_id) REFERENCES "user" (user_id),
-    CONSTRAINT yearly_summary_category_category_id_fk FOREIGN KEY (category_id) REFERENCES "category" (category_id)
+    owner_user_id uuid NOT NULL,
+    CONSTRAINT summary_category_owner_user_id_fk FOREIGN KEY (owner_user_id) REFERENCES "user" (user_id),
+    CONSTRAINT summary_category_category_id_fk FOREIGN KEY (category_id) REFERENCES "category" (category_id)
 );
-CREATE INDEX summary_category_user_id_idx ON summary_category (user_id);
+CREATE INDEX summary_category_user_id_idx ON summary_category (owner_user_id);
