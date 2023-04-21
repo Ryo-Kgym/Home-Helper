b525dcedee4c3f79c706743effcc35d1a78a6acf
Ryo-Kgym <mraytoh@icloud.com>
2023-04-21 23:01:55 +0900

    Update the useGroup.ts

--- home-helper-front/src/hooks/group/useGroup.ts
+++ home-helper-front/src/hooks/group/useGroup.ts
@@ -7,5 +7,5 @@ export const useGroup = () => {
   const save = (group: { id: string; name: string }) => {
     setGroup(group);
   };
-  return { id: group.id, name: group.name, save };
+  return { groupId: group.id, groupName: group.name, save };
 };
