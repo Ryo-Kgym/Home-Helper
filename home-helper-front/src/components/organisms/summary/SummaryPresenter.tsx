cf69571ba8a9baa9b10e59e1fd72e5b917cd8e40
Ryo-Kgym <mraytoh@icloud.com>
2023-04-28 14:15:04 +0900

    Used the year picker.

--- home-helper-front/src/components/organisms/summary/SummaryPresenter.tsx
+++ home-helper-front/src/components/organisms/summary/SummaryPresenter.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
 import { Tab, TabProps } from "@components/atoms/Tab";
+import { YearPicker } from "@components/atoms/YearPicker";
 
 type SummaryPresenterProps = {
+  year: Date | null;
+  setYear: (year: Date | null) => void;
   defaultSelect: string;
   tabPropsList: TabProps[];
 };
 
 export const SummaryPresenter: FC<SummaryPresenterProps> = ({
+  year,
+  setYear,
   defaultSelect,
   tabPropsList,
 }) => (
   <div>
+    <YearPicker year={year} setYear={setYear} />
     <Tab defaultSelect={defaultSelect} tabPropsList={tabPropsList} />
   </div>
 );
