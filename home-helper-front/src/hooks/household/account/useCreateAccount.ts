bdbd2a5d9102d0e6976dc80b4812bd668a6c86bc
Ryo-Kgym <mraytoh@icloud.com>
2023-04-30 00:36:00 +0900

    Created the useCreateAccount.

---
+++ home-helper-front/src/hooks/household/account/useCreateAccount.ts
@@ -0,0 +1,29 @@
+import { useUuid } from "@hooks/uuid/useUuid";
+import { useGroup } from "@hooks/group/useGroup";
+import { useCreateAccountMutation } from "@graphql/postgraphile/generated/graphql";
+
+type CreateAccountArgs = {
+  accountName: string;
+  displayOrder: number;
+};
+
+export const useCreateAccount = ({
+  accountName,
+  displayOrder,
+}: CreateAccountArgs) => {
+  const { get } = useUuid();
+  const { groupId } = useGroup();
+
+  const [ignore, mutation] = useCreateAccountMutation();
+
+  const registerAccount = () => {
+    mutation({
+      accountId: get(),
+      accountName,
+      displayOrder,
+      groupId,
+    });
+  };
+
+  return { registerAccount };
+};
