c72bbc73656d7872e54cfb701307a98c5bae6a87
Ryo-Kgym <mraytoh@icloud.com>
2023-04-29 23:18:56 +0900

    Used the DailyTable.

--- home-helper-front/src/components/organisms/daily_table/account/DailyTableByAccountPresenter.tsx
+++ home-helper-front/src/components/organisms/daily_table/account/DailyTableByAccountPresenter.tsx
@@ -1,24 +1,22 @@
 import { FC } from "react";
 import { TableProps } from "@components/atoms/Table";
-import { DailySearchContainer } from "@components/organisms/daily_table/DailySearchContainer";
+import { DailyTable } from "@components/organisms/daily_table/DailyTable";
 
 type DailyTableByCategoryPresenterProps = {
-  firstDay: Date;
-  lastDay: Date;
   tableProps: TableProps[];
   incomeTotal: number | undefined;
   outcomeTotal: number | undefined;
 };
 export const DailyTableByAccountPresenter: FC<
   DailyTableByCategoryPresenterProps
-> = ({ firstDay, lastDay, tableProps, incomeTotal, outcomeTotal }) => {
+> = ({ tableProps, incomeTotal, outcomeTotal }) => {
   return (
-    <DailySearchContainer
-      dailyDetail={tableProps}
-      defaultFromDate={firstDay}
-      defaultToDate={lastDay}
-      defaultIncomeTotal={incomeTotal}
-      defaultOutcomeTotal={outcomeTotal}
-    />
+    <div>
+      <DailyTable
+        tablePropsList={tableProps}
+        incomeTotal={incomeTotal}
+        outcomeTotal={outcomeTotal}
+      />
+    </div>
   );
 };
