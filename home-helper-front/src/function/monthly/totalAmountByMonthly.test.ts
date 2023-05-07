c36ea58a1caf4857683b536599840dcc0d463263
Ryo-Kgym <mraytoh@icloud.com>
2023-05-07 23:42:39 +0900

    Added the test case.

--- home-helper-front/src/function/monthly/totalAmountByMonthly.test.ts
+++ home-helper-front/src/function/monthly/totalAmountByMonthly.test.ts
@@ -16,7 +16,6 @@ describe("totalAmountByMonthly", () => {
         { month: "2023-03", amount: 300 },
         { month: "2023-04", amount: 400 },
         { month: "2023-05", amount: 500 },
-        { month: "2023-07", amount: 700 },
         { month: "2023-08", amount: 800 },
         { month: "2023-09", amount: 900 },
         { month: "2023-10", amount: 1000 },
@@ -26,13 +25,12 @@ describe("totalAmountByMonthly", () => {
         { month: "2023-04", amount: 400 },
         { month: "2023-05", amount: 500 },
         { month: "2023-06", amount: 600 },
-        { month: "2023-07", amount: 700 },
         { month: "2023-08", amount: 800 },
         { month: "2023-09", amount: 900 },
         { month: "2023-10", amount: 1000 },
         { month: "2023-11", amount: 1100 },
       ],
-      fromDate: new Date("2023-01-01"),
+      fromDate: new Date("2022-10-01"),
       toDate: new Date("2023-12-31"),
     },
   ])(
@@ -42,9 +40,9 @@ describe("totalAmountByMonthly", () => {
       expect(actual).toEqual({
         key,
         monthlyTotal: [
-          100, 400, 600, 800, 1000, 600, 1400, 1600, 1800, 2000, 2200, 0,
+          0, 0, 0, 100, 400, 600, 800, 1000, 600, 0, 1600, 1800, 2000, 2200, 0,
         ],
-        total: 12500,
+        total: 11100,
       });
     }
   );
