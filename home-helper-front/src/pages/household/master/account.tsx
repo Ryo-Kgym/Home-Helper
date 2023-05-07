4c1cddbe22a0986be463a823fee5b0ceea2e95a7
Ryo-Kgym <mraytoh@icloud.com>
2023-05-07 22:19:15 +0900

    Deleted the master page.

--- home-helper-front/src/pages/household/master/account.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from "next";
-import { Page } from "@components/page/master/MasterAccountPage";
-
-const index: NextPage = () => {
-  return <Page />;
-};
-
-export default index;
