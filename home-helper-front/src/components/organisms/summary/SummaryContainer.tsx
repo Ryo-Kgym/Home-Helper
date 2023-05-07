54cbcb9f9d26d7e61218045757fe7d2856931a24
Ryo-Kgym <mraytoh@icloud.com>
2023-05-07 22:15:52 +0900

    Moved the setting to the summary page.

--- home-helper-front/src/components/organisms/summary/SummaryContainer.tsx
+++ home-helper-front/src/components/organisms/summary/SummaryContainer.tsx
@@ -1,14 +1,17 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { SummaryPresenter } from "@components/organisms/summary/SummaryPresenter";
 import { TabProps } from "@components/atoms/Tab";
 import { MonthlyCategory } from "@components/organisms/summary/monthly_category";
 import { useFetchSummaryCategoryAmountByUser } from "@hooks/household/summary_category/useFetchSummaryCategoryAmountByUser";
 import { useUser } from "@hooks/user/useUser";
+import { SummaryCategory } from "@components/organisms/master/summary_category";
 
 type SummaryContainerProps = {};
 
 export const SummaryContainer: FC<SummaryContainerProps> = () => {
   const [year, setYear] = useState<Date | null>(new Date());
+  const [displayUserName, setDisplayUserName] = useState<string>("");
+
   const { userName } = useUser();
 
   const { data } = useFetchSummaryCategoryAmountByUser({
@@ -18,7 +21,7 @@ export const SummaryContainer: FC<SummaryContainerProps> = () => {
   const tabProps: TabProps[] = [
     {
       value: "monthlyCategory",
-      label: `月次カテゴリ（${userName}）`,
+      label: `月次カテゴリ（${displayUserName}）`,
       contents: <MonthlyCategory data={data} />,
     },
     {
@@ -26,7 +29,16 @@ export const SummaryContainer: FC<SummaryContainerProps> = () => {
       label: "月次カテゴリ（全体）",
       contents: <div>工事中</div>,
     },
+    {
+      value: "setting",
+      label: "設定",
+      contents: <SummaryCategory />,
+    },
   ];
+
+  useEffect(() => {
+    setDisplayUserName(userName);
+  }, []);
 
   return (
     <SummaryPresenter
