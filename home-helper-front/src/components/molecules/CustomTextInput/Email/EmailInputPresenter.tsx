ce0ea18a46183a14dd69e37fa3c957a0342e929e
Ryo-Kgym <mraytoh@icloud.com>
2023-05-14 01:13:02 +0900

    Added the disabled props.

--- home-helper-front/src/components/molecules/CustomTextInput/Email/EmailInputPresenter.tsx
+++ home-helper-front/src/components/molecules/CustomTextInput/Email/EmailInputPresenter.tsx
@@ -4,11 +4,13 @@ import { FC } from "react";
 type EmailInputPresenterProps = {
   email: string;
   setEmail: (email: string) => void;
+  disabled?: boolean;
 };
 
 export const EmailInputPresenter: FC<EmailInputPresenterProps> = ({
   email,
   setEmail,
+  disabled = false,
 }) => (
   <TextInput
     label={"E-mail"}
@@ -17,5 +19,6 @@ export const EmailInputPresenter: FC<EmailInputPresenterProps> = ({
     placeholder={"xxx@yyy.zzz"}
     withAsterisk={true}
     maxLength={256}
+    disabled={disabled}
   />
 );
