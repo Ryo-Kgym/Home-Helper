1be5c90ed1bf83107163cf9196303ea47818bd57
Ryo-Kgym <mraytoh@icloud.com>
2023-05-13 20:34:37 +0900

    Created the sample google login.

--- home-helper-front/src/components/organisms/login/LoginPresenter.tsx
+++ home-helper-front/src/components/organisms/login/LoginPresenter.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { GoogleLoginContainer } from "@components/organisms/login/google/GoogleLoginContainer";
 
 type LoginPresenterProps = {};
 export const LoginPresenter: FC<LoginPresenterProps> = () => (
@@ -14,12 +15,9 @@ export const LoginPresenter: FC<LoginPresenterProps> = () => (
           >
             <h3 className="text-2xl font-bold">Login &rarr;</h3>
           </a>
-          <a
-            href="/top"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Sign Up &rarr;</h3>
-          </a>
+          <div>
+            <GoogleLoginContainer />
+          </div>
         </div>
       </main>
     </div>
--- home-helper-front/src/pages/_app.tsx
+++ home-helper-front/src/pages/_app.tsx
@@ -7,20 +7,26 @@ import { Provider } from "urql";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "@provider/Authentication/AuthProvider";
 import { RecoilRoot } from "recoil";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return (
-    <RecoilRoot>
-      <AuthProvider>
-        <Provider value={client}>
-          <MantineProvider withNormalizeCSS withGlobalStyles>
-            <NotificationsProvider>
-              <Component {...pageProps} />
-              <Toaster />
-            </NotificationsProvider>
-          </MantineProvider>
-        </Provider>
-      </AuthProvider>
-    </RecoilRoot>
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <AuthProvider>
+          <Provider value={client}>
+            <MantineProvider withNormalizeCSS withGlobalStyles>
+              <NotificationsProvider>
+                <Component {...pageProps} />
+                <Toaster />
+              </NotificationsProvider>
+            </MantineProvider>
+          </Provider>
+        </AuthProvider>
+      </RecoilRoot>
+    </SessionProvider>
   );
 }
