061db38a31c58e14320ab72e583fd309a18919d9
Ryo-Kgym <mraytoh@icloud.com>
2023-05-13 11:11:19 +0900

    Reviewed the category page.

--- home-helper-front/src/components/page/category/CategoryPage.tsx
+++ home-helper-front/src/components/page/category/CategoryPage.tsx
@@ -1,17 +1,11 @@
 import { HouseHoldLayout } from "@components/atoms/Layout/HouseHoldLayout";
 import { FC } from "react";
-import { Split } from "@components/atoms/Split";
 import { CategoryContainer } from "@components/organisms/category/CategoryContainer";
-import { CalendarContainer } from "@components/organisms/calendar/CalendarContainer";
 
 export const Page: FC = () => {
   return (
     <HouseHoldLayout>
-      <Split
-        size={35}
-        first={<CalendarContainer />}
-        second={<CategoryContainer />}
-      />
+      <CategoryContainer />
     </HouseHoldLayout>
   );
 };
