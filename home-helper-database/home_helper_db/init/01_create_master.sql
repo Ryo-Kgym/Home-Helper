882b73090361937e16a6166885a0ad593c7010d4
Ryo-Kgym <mraytoh@icloud.com>
2023-09-10 22:28:25 +0900

    issue/39 - スキーマ名を変更した。

--- home-helper-database/home_helper_db/init/01_create_master.sql
+++ home-helper-database/home_helper_db/init/01_create_master.sql
@@ -1,5 +1,5 @@
-drop table if exists home_helper.help_item cascade;
-create table home_helper.help_item (
+drop table if exists helper_kids.help_item cascade;
+create table helper_kids.help_item (
     help_item_id uuid          not null primary key,
     name         varchar(32)   not null,
     point        integer       not null,
@@ -8,15 +8,15 @@ create table home_helper.help_item (
     constraint fk_help_item_group_id foreign key (group_id) references "group" (group_id)
 );
 
-comment on table home_helper.help_item is 'お手伝い項目マスタ';
-comment on column home_helper.help_item.help_item_id is 'ID';
-comment on column home_helper.help_item.name is '項目名';
-comment on column home_helper.help_item.point is 'お手伝いポイント';
-comment on column home_helper.help_item.memo is 'メモ';
-comment on column home_helper.help_item.group_id is 'グループID';
+comment on table helper_kids.help_item is 'お手伝い項目マスタ';
+comment on column helper_kids.help_item.help_item_id is 'ID';
+comment on column helper_kids.help_item.name is '項目名';
+comment on column helper_kids.help_item.point is 'お手伝いポイント';
+comment on column helper_kids.help_item.memo is 'メモ';
+comment on column helper_kids.help_item.group_id is 'グループID';
 
-drop table if exists home_helper.exchange_item cascade;
-create table home_helper.exchange_item (
+drop table if exists helper_kids.exchange_item cascade;
+create table helper_kids.exchange_item (
     exchange_item_id uuid          not null primary key,
     name             varchar(32)   not null,
     point            integer       not null,
@@ -25,15 +25,15 @@ create table home_helper.exchange_item (
     constraint fk_exchange_item_group_id foreign key (group_id) references "group" (group_id)
 );
 
-comment on table home_helper.exchange_item is '交換品マスタ';
-comment on column home_helper.exchange_item.exchange_item_id is 'ID';
-comment on column home_helper.exchange_item.name is '品名';
-comment on column home_helper.exchange_item.point is '交換ポイント';
-comment on column home_helper.exchange_item.memo is 'メモ';
-comment on column home_helper.exchange_item.group_id is 'グループID';
+comment on table helper_kids.exchange_item is '交換品マスタ';
+comment on column helper_kids.exchange_item.exchange_item_id is 'ID';
+comment on column helper_kids.exchange_item.name is '品名';
+comment on column helper_kids.exchange_item.point is '交換ポイント';
+comment on column helper_kids.exchange_item.memo is 'メモ';
+comment on column helper_kids.exchange_item.group_id is 'グループID';
 
-drop table if exists home_helper.helper_kid cascade;
-create table home_helper.helper_kid (
+drop table if exists helper_kids.helper_kid cascade;
+create table helper_kids.helper_kid (
     helper_kid_id uuid                      not null primary key,
     name          varchar(32)               not null,
     name_suffix   varchar(8) default 'さん' not null,
@@ -41,8 +41,8 @@ create table home_helper.helper_kid (
     constraint fk_helper_kid_group_id foreign key (group_id) references "group" (group_id)
 );
 
-comment on table home_helper.helper_kid is 'お手伝いキッズマスタ';
-comment on column home_helper.helper_kid.helper_kid_id is 'ID';
-comment on column home_helper.helper_kid.name is '氏名';
-comment on column home_helper.helper_kid.name_suffix is '名前 接尾辞';
-comment on column home_helper.helper_kid.group_id is 'グループID';
+comment on table helper_kids.helper_kid is 'お手伝いキッズマスタ';
+comment on column helper_kids.helper_kid.helper_kid_id is 'ID';
+comment on column helper_kids.helper_kid.name is '氏名';
+comment on column helper_kids.helper_kid.name_suffix is '名前 接尾辞';
+comment on column helper_kids.helper_kid.group_id is 'グループID';
