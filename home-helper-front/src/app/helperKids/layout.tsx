c2c9f9c687f4719926dacf39bbcf50310cacf597
Ryo-Kgym <mraytoh@icloud.com>
2023-09-27 14:37:21 +0900

    issue/115 - HelperKidsLayout の仕様変更

--- home-helper-front/src/app/helperKids/layout.tsx
+++ home-helper-front/src/app/helperKids/layout.tsx
@@ -2,11 +2,8 @@
  * Copyright (c) 2023 Ryo-Kgym.
  */
 
-import { ReactNode } from "react";
 import { HelperKidsLayout } from "./_layout/HelperKidsLayout";
 
-const Layout = ({ children }: { children: ReactNode }) => (
-  <HelperKidsLayout>{children}</HelperKidsLayout>
-);
+const Layout = HelperKidsLayout;
 
 export default Layout;
