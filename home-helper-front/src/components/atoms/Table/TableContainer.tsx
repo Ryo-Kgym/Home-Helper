b6773d775bf652f232f4db938538929ef4b4d927
Ryo-Kgym <mraytoh@icloud.com>
2023-09-27 16:07:04 +0900

    issue/115 - バージョンアップに伴う仕様変更

--- home-helper-front/src/components/atoms/Table/TableContainer.tsx
+++ home-helper-front/src/components/atoms/Table/TableContainer.tsx
@@ -1,8 +1,10 @@
-import { FC, useRef, useState } from "react";
-import { TablePresenter } from "@components/atoms/Table/TablePresenter";
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
 import { ColumnProps, TableProps } from "@components/atoms/Table/index";
-import { createStyles } from "@mantine/core";
 import { MantineSize } from "@mantine/styles";
+import { Table } from "@mantine/core";
 
 type Props = {
   header: string[];
@@ -13,166 +15,63 @@ type Props = {
   size?: MantineSize;
   toBottom?: boolean;
 };
-export const TableContainer: FC<Props> = ({
-  header,
-  tablePropsList,
-  footer,
-  height = "80vh",
-  size = "xl",
-  toBottom = false,
-}) => {
-  const [scrolled, setScrolled] = useState(false);
-  const [toButtonOpen, setToButtonOpen] = useState(false);
-  const { classes, cx } = useStyles();
-
-  const thead =
-    header.length > 0 ? (
-      <tr>
-        {header.map((title, i) => {
-          return <th key={"th" + i}>{title}</th>;
-        })}
-      </tr>
-    ) : undefined;
+export const TableContainer = ({ header, tablePropsList, footer }: Props) => {
+  const thead = (
+    <Table.Tr>
+      {header.map((title, i) => (
+        <Table.Th key={"th" + i}>{title}</Table.Th>
+      ))}
+    </Table.Tr>
+  );
 
   const generateRow = (
     { keyPrefix, onClick, columns }: TableProps,
-    i: number
+    i: number,
   ) => (
-    <tr
+    <Table.Tr
       key={keyPrefix + i}
       onClick={onClick ?? (() => {})}
       className={onClick ? "cursor-pointer" : ""}
     >
       {columns.map(generateColumn)}
-    </tr>
+    </Table.Tr>
   );
 
   const generateColumn = (columnProps: ColumnProps, j: number) => (
-    <td
+    <Table.Td
       key={"td" + j}
       align={columnProps.align ?? "left"}
       hidden={columnProps.hidden ?? false}
     >
       {columnProps.value}
-    </td>
+    </Table.Td>
   );
 
   const generateFooterRow = (row: ColumnProps[], i: number) => (
-    <tr key={"tfoot" + i}>
+    <Table.Tr key={"tfoot" + i}>
       {row.map((columnProps: ColumnProps, j: number) => (
-        <td
+        <Table.Td
           key={"td" + j}
           align={columnProps.align ?? "left"}
           hidden={columnProps.hidden ?? false}
           className={"p-2 border-2 font-bold bg-white"}
         >
           {columnProps.value}
-        </td>
+        </Table.Td>
       ))}
-    </tr>
+    </Table.Tr>
   );
 
   const tbody = <>{tablePropsList.map(generateRow)}</>;
-
   const tfoot = footer ? <>{footer.map(generateFooterRow)}</> : undefined;
 
-  const viewport = useRef<HTMLDivElement>(null);
-
-  const scrollToBottom = toBottom
-    ? () =>
-        viewport?.current?.scrollTo({
-          top: viewport.current.scrollHeight,
-          behavior: "smooth",
-        })
-    : undefined;
-
-  const onMouseMoveHandler = () => {
-    setToButtonOpen(true);
-    setTimeout(() => {
-      setToButtonOpen(false);
-    }, 2000);
-  };
-
   return (
-    <TablePresenter
-      headerTr={thead}
-      tbody={tbody}
-      tfoot={tfoot}
-      height={height}
-      scrolled={scrolled}
-      setScrolled={setScrolled}
-      classes={classes}
-      cx={cx}
-      fontSize={paddingMap.get(size)!.fontSize}
-      horizontalSpacing={paddingMap.get(size)!.horizontalSpacing}
-      verticalSpacing={paddingMap.get(size)!.verticalSpacing}
-      viewport={viewport}
-      scrollToBottom={scrollToBottom}
-      toButtonOpen={toButtonOpen}
-      onMouseMoveHandler={onMouseMoveHandler}
-      onMouseOutHandler={() => {
-        setToButtonOpen(false);
-      }}
-    />
+    <Table.ScrollContainer minWidth={"100%"} type={"native"}>
+      <Table striped highlightOnHover withColumnBorders withTableBorder>
+        {thead && <Table.Thead>{thead}</Table.Thead>}
+        <Table.Tbody>{tbody}</Table.Tbody>
+        {tfoot && <Table.Tfoot>{tfoot}</Table.Tfoot>}
+      </Table>
+    </Table.ScrollContainer>
   );
 };
-
-const useStyles = createStyles((theme) => ({
-  header: {
-    position: "sticky",
-    top: 0,
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-    transition: "box-shadow 150ms ease",
-    zIndex: 5,
-
-    "&::after": {
-      content: '""',
-      position: "absolute",
-      left: 0,
-      right: 0,
-      bottom: 0,
-      borderBottom: `1px solid ${
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[3]
-          : theme.colors.gray[2]
-      }`,
-    },
-  },
-  footer: {
-    position: "sticky",
-    bottom: 0,
-    backgroundColor:
-      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
-    transition: "box-shadow 150ms ease",
-    zIndex: 5,
-
-    "&::after": {
-      content: '""',
-      position: "absolute",
-      left: 0,
-      right: 0,
-      bottom: 0,
-      borderBottom: `1px solid ${
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[3]
-          : theme.colors.gray[2]
-      }`,
-    },
-  },
-
-  scrolled: {
-    boxShadow: theme.shadows.sm,
-  },
-}));
-
-const paddingMap = new Map<
-  MantineSize,
-  { fontSize: number; horizontalSpacing: number; verticalSpacing: number }
->([
-  ["xs", { fontSize: 14, horizontalSpacing: 10, verticalSpacing: 10 }],
-  ["sm", { fontSize: 18, horizontalSpacing: 15, verticalSpacing: 15 }],
-  ["md", { fontSize: 20, horizontalSpacing: 20, verticalSpacing: 20 }],
-  ["lg", { fontSize: 23, horizontalSpacing: 25, verticalSpacing: 25 }],
-  ["xl", { fontSize: 28, horizontalSpacing: 30, verticalSpacing: 30 }],
-]);
