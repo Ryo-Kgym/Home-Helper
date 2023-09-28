a694861a1b8b8d3c99cfab4bf0d5d56a08271b81
Ryo-Kgym <mraytoh@icloud.com>
2023-09-28 13:17:34 +0900

    issue/121 - Select をまとめた。

--- home-helper-front/src/components/atoms/Select/index.ts
+++ home-helper-front/src/components/atoms/Select/index.ts
@@ -1,1 +1,11 @@
-export { SelectContainer as Select } from "./SelectContainer";
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
+import {
+  ComboboxItemGroup,
+  ComboboxItem,
+} from "@mantine/core/lib/components/Combobox/Combobox.types";
+
+export { MantineV7Select as Select } from "./MantineV7Select";
+export type SelectData = ComboboxItem | ComboboxItemGroup;
--- home-helper-front/src/components/atoms/index.ts
+++ home-helper-front/src/components/atoms/index.ts
@@ -1,5 +1,10 @@
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
 export * from "./Button";
 export * from "./NavLink";
 export * from "./Status/Validity";
+export * from "./Select";
 
 export type HomeHelperSize = "pc" | "tablet" | "mobile";
