e65b1ec79de6b71e9929504cc638a79c2846a5aa
Ryo-Kgym <mraytoh@icloud.com>
2023-10-10 01:45:04 +0900

    issue/121 - 拡張子の変更

--- home-helper-front/src/components/ui/index.ts
+++ home-helper-front/src/components/ui/index.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 Ryo-Kgym.
  */
 
-export * from "./Button";
+export * from "./Button/index";
 export * from "./Select";
 export * from "./TransferList";
 export * from "./Tabs";
