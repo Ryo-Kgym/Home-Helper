4f226bdf482931d98fab3082931753ad6adeb770
Ryo-Kgym <mraytoh@icloud.com>
2023-11-04 14:22:24 +0900

    issue/130 - uiのexportを追加

--- home-helper-front/src/components/ui/index.ts
+++ home-helper-front/src/components/ui/index.ts
@@ -3,6 +3,8 @@
  */
 
 export * from "./Button/index";
+export * from "@components/atoms/Modal";
 export * from "./Select";
 export * from "./TransferList";
 export * from "./Tabs";
+export * from "@components/atoms/Table";
