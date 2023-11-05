02941e788e36de4fd0b7805ffb708936a87fc29a
Ryo-Kgym <mraytoh@icloud.com>
2023-11-06 00:43:12 +0900

    no ticket - FC削除

--- home-helper-front/src/components/molecules/Total/IocomeTotalContainer.tsx
+++ home-helper-front/src/components/molecules/Total/IocomeTotalContainer.tsx
@@ -1,14 +1,16 @@
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
 import { IocomeTotalPresenter } from "@components/molecules/Total/IocomeTotalPresenter";
 import { FC } from "react";
 
-type IocomeTotalContainerProps = {
-  income: number | undefined;
-  outcome: number | undefined;
-};
-
-export const IocomeTotalContainer: FC<IocomeTotalContainerProps> = ({
+export const IocomeTotalContainer = ({
   income = 0,
   outcome = 0,
+}: {
+  income: number | undefined;
+  outcome: number | undefined;
 }) => {
   const hidden = income === 0 && outcome === 0;
 
