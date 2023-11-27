74b48a4c64c3436ac73c326c1f14801dc4ec6f5c
Ryo-Kgym <mraytoh@icloud.com>
2023-11-28 00:37:45 +0900

    全てのdetailを集積するviewを作成した。

--- home-helper-database/household_db/init/03.create_view.sql
+++ home-helper-database/household_db/init/03.create_view.sql
@@ -134,3 +134,31 @@ order by
     d.account_id,
     d.withdrawal_date
 ;
+
+DROP VIEW IF EXISTS all_detail_view;
+CREATE VIEW all_detail_view AS
+SELECT dd.id,
+       'daily'                                                            as type,
+       dd.date,
+       dd.genre_id,
+       dd.iocome_type,
+       dd.category_id,
+       dd.account_id,
+       dd.amount                                                          as original_amount,
+       case when dd.iocome_type = 'INCOME' then 1 else -1 end * dd.amount as signed_amount,
+       dd.memo
+FROM daily_detail dd
+UNION ALL
+SELECT cd.id,
+       'credit_card'                                                      as type,
+       cd.date,
+       cd.genre_id,
+       cd.iocome_type,
+       cd.category_id,
+       cs.account_id,
+       cd.amount                                                          as original_amount,
+       case when cd.iocome_type = 'INCOME' then 1 else -1 end * cd.amount as signed_amount,
+       cd.memo
+FROM credit_card_detail cd
+         inner join credit_card_summary cs on cd.summary_id = cs.id
+order by date;
