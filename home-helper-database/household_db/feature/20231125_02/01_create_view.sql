fb79d548c7762359d9b98fbaa633b2828b483fd2
Ryo-Kgym <mraytoh@icloud.com>
2023-11-28 21:14:53 +0900

    - create or replaceに変更 - group_idを追加 - dateの定義を変更

--- home-helper-database/household_db/feature/20231125_02/01_create_view.sql
+++ home-helper-database/household_db/feature/20231125_02/01_create_view.sql
@@ -1,5 +1,4 @@
-DROP VIEW IF EXISTS all_detail_view;
-CREATE VIEW all_detail_view AS
+CREATE OR REPLACE VIEW all_detail_view AS
 SELECT dd.id,
        'daily'                                                            as type,
        dd.date,
@@ -9,19 +8,21 @@ SELECT dd.id,
        dd.account_id,
        dd.amount                                                          as original_amount,
        case when dd.iocome_type = 'INCOME' then 1 else -1 end * dd.amount as signed_amount,
-       dd.memo
+       dd.memo,
+       dd.group_id
 FROM daily_detail dd
 UNION ALL
 SELECT cd.id,
        'credit_card'                                                      as type,
-       cd.date,
+       cs.withdrawal_date                                                 as date,
        cd.genre_id,
        cd.iocome_type,
        cd.category_id,
        cs.account_id,
        cd.amount                                                          as original_amount,
        case when cd.iocome_type = 'INCOME' then 1 else -1 end * cd.amount as signed_amount,
-       cd.memo
+       cd.memo,
+       cd.group_id
 FROM credit_card_detail cd
          inner join credit_card_summary cs on cd.summary_id = cs.id
 order by date;
