b6992b33ab7144672807e811419c2a33dd7338a6
Ryo-Kgym <mraytoh@icloud.com>
2023-12-06 22:59:04 +0900

    Home-Helper-140 - Spring Batch環境を構築した。

---
+++ home-helper-migration/src/main/java/home/helper/migration/HomeHelperMigrationApplication.java
@@ -0,0 +1,13 @@
+package home.helper.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeHelperMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeHelperMigrationApplication.class, args);
+	}
+
+}
---
+++ home-helper-migration/src/test/java/home/helper/migration/HomeHelperMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package home.helper.migration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHelperMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
