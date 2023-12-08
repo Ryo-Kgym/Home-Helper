cf4e2c70ec7573b3adea6637f4510054e550a698
Ryo-Kgym <mraytoh@icloud.com>
2023-12-09 02:08:10 +0900

    Home-Helper-140 - プロジェクト名の変更

--- home-helper-batch/src/main/java/home/helper/batch/HomeHelperBatchApplication.java
+++ home-helper-batch/src/main/java/home/helper/batch/HomeHelperBatchApplication.java
@@ -1,13 +1,17 @@
-package home.helper.migration;
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
+package home.helper.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HomeHelperMigrationApplication {
+public class HomeHelperBatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HomeHelperMigrationApplication.class, args);
+		SpringApplication.run(HomeHelperBatchApplication.class, args);
 	}
 
 }
--- home-helper-batch/src/test/java/home/helper/batch/HomeHelperBatchApplicationTests.java
+++ home-helper-batch/src/test/java/home/helper/batch/HomeHelperBatchApplicationTests.java
@@ -1,10 +1,14 @@
-package home.helper.migration;
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
+package home.helper.batch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HomeHelperMigrationApplicationTests {
+class HomeHelperBatchApplicationTests {
 
 	@Test
 	void contextLoads() {
