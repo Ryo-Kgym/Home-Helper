a94e9d2ae84fef5b3f1800987dea2975a8e65121
Ryo-Kgym <mraytoh@icloud.com>
2023-12-10 21:45:42 +0900

    Home-Helper-142 - useGetMonthlyDeposit を呼び出した。

--- home-helper-front/src/components/organisms/deposit/DepositContainer.tsx
+++ home-helper-front/src/components/organisms/deposit/DepositContainer.tsx
@@ -3,7 +3,16 @@
  */
 
 import { DepositPresenter } from "./DepositPresenter";
+import { useGetMonthlyDeposit } from "@hooks/household/deposit/useGetMonthlyDeposit";
 
 export const DepositContainer = () => {
-  return <DepositPresenter />;
+  const thisYear = new Date().getFullYear();
+  const { monthlyDeposits, loading, error } = useGetMonthlyDeposit({
+    fromDate: new Date(thisYear, 0, 1),
+    toDate: new Date(thisYear, 11, 31),
+  });
+
+  if (loading) return <div>loading...</div>;
+
+  return <DepositPresenter monthlyCategories={monthlyDeposits} />;
 };
--- home-helper-front/src/components/organisms/deposit/DepositPresenter.tsx
+++ home-helper-front/src/components/organisms/deposit/DepositPresenter.tsx
@@ -2,4 +2,10 @@
  * Copyright (c) 2023 Ryo-Kgym.
  */
 
-export const DepositPresenter = () => <div></div>;
+import { MonthlyDeposit } from "@hooks/household/deposit/monthlyDeposit";
+
+export const DepositPresenter = ({
+  monthlyCategories,
+}: {
+  monthlyCategories: MonthlyDeposit[];
+}) => <div></div>;
