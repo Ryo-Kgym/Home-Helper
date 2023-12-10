d0ee2998795ba905b7f6edba3eb440d780ba0241
Ryo-Kgym <mraytoh@icloud.com>
2023-12-10 21:31:09 +0900

    Home-Helper-142 - 指定した日付間のyyyy-mm形式の文字列配列を返す関数を作成した。

---
+++ home-helper-front/src/function/date/generate-year-month-list.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 Ryo-Kgym.
+ */
+
+/**
+ * Generates a list of year-month string values between two given dates.
+ *
+ * @param {Date} fromDate - The starting date.
+ * @param {Date} toDate - The ending date.
+ * @returns {Array} - A list of year-month string values.
+ */
+export const generateYearMonthList = (fromDate: Date, toDate: Date) => {
+  if (fromDate > toDate) {
+    throw new Error("fromDate must be less than or equal to toDate");
+  }
+
+  const yearMonths = [fromDate.toISOString().slice(0, 7)];
+  let yearMonthVariable = yearMonths[0];
+
+  while (yearMonthVariable !== toDate.toISOString().slice(0, 7)) {
+    const year = parseInt(yearMonthVariable.slice(0, 4));
+    const month = parseInt(yearMonthVariable.slice(5, 7));
+    if (month === 12) {
+      yearMonthVariable = `${year + 1}-01`;
+    } else {
+      yearMonthVariable = `${year}-` + (month + 1).toString().padStart(2, "0");
+    }
+    yearMonths.push(yearMonthVariable);
+  }
+
+  return yearMonths;
+};
