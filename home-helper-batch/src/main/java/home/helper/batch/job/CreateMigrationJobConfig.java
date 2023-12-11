b59f58b42c469836a2c406ec434e7913a74f6f1b
Ryo-Kgym <mraytoh@icloud.com>
2023-12-11 12:42:09 +0900

    Home-Helper-145 - 一時的なサンプル

--- home-helper-batch/src/main/java/home/helper/batch/job/CreateMigrationJobConfig.java
+++ home-helper-batch/src/main/java/home/helper/batch/job/CreateMigrationJobConfig.java
@@ -4,37 +4,44 @@
 
 package home.helper.batch.job;
 
+import home.helper.batch.persistence.migration.household.DbMigrationUser;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.batch.MyBatisCursorItemReader;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
 public class CreateMigrationJobConfig {
-    private final JobBuilderFactory jobBuilderFactory;
-    private final StepBuilderFactory stepBuilderFactory;
-    private final ItemProcessor<User, User> processor;
-    private final ItemWriter<User> writer;
+    private final PlatformTransactionManager transactionManager;
+    private final JobRepository jobRepository;
+    private final SqlSessionFactory sqlSessionFactory;
 
     @Bean
     public Job userJob(Step userStep) {
-        return jobBuilderFactory.get("userJob")
+        return new JobBuilder("userJob", jobRepository)
                 .start(userStep)
                 .build();
     }
 
     @Bean
-    public Step userStep(ItemReader<User> reader) {
-        return stepBuilderFactory.get("userStep")
-                .<User, User>chunk(10)
+    public Step userStep(
+            ItemReader<DbMigrationUser> reader,
+            ItemProcessor<DbMigrationUser, DbMigrationUser> processor,
+            ItemWriter<DbMigrationUser> writer
+    ) {
+        return new StepBuilder("userStep", jobRepository)
+                .<DbMigrationUser, DbMigrationUser>chunk(100, transactionManager)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
@@ -42,24 +49,20 @@ public class CreateMigrationJobConfig {
     }
 
     @Bean
-    public ItemReader<User> reader() {
-        MyBatisCursorItemReader<User> reader = new MyBatisCursorItemReader<>();
-        reader.setQueryId("home.helper.batch.job.UserMapper.selectAll");
+    public ItemReader<DbMigrationUser> reader() {
+        MyBatisCursorItemReader<DbMigrationUser> reader = new MyBatisCursorItemReader<>();
+        reader.setQueryId("home.helper.batch.persistence.migration.household.SelectMigrationUserMapper.selectMigrationUser");
         reader.setSqlSessionFactory(sqlSessionFactory);
         return reader;
     }
 
     @Bean
-    public ItemProcessor<User, User> processor() {
-        return user ->
-                User.builder()
-                .name(user.getName())
-                .age(user.getAge() + 1)
-                .build();
+    public ItemProcessor<DbMigrationUser, DbMigrationUser> processor() {
+        return user -> user;
     }
 
     @Bean
-    public ItemWriter<User> writer() {
+    public ItemWriter<DbMigrationUser> writer() {
         return users -> users.forEach(System.out::println);
     }
 }
