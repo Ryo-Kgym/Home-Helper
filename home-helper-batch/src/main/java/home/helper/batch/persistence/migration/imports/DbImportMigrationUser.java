63b6f8db9519b1e6342b2b41d2a6b4257752fa3b
Ryo-Kgym <mraytoh@icloud.com>
2023-12-13 23:37:13 +0900

    Home-Helper-147 - lombokの見直し

--- home-helper-batch/src/main/java/home/helper/batch/persistence/migration/imports/DbImportMigrationUser.java
+++ home-helper-batch/src/main/java/home/helper/batch/persistence/migration/imports/DbImportMigrationUser.java
@@ -4,15 +4,11 @@
 
 package home.helper.batch.persistence.migration.imports;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Builder
 @Value
-@NoArgsConstructor(force = true)
-@AllArgsConstructor
 public class DbImportMigrationUser {
     private String userId;
     private String userName;
