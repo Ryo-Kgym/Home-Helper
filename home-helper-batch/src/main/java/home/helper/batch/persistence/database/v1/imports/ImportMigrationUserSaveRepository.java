4f8d8ceef05d7d52a665a0ae0c45172164e5af9e
Ryo-Kgym <mraytoh@icloud.com>
2023-12-17 06:58:03 +0900

    Home-Helper-119 - user を登録する処理を実装した。

--- home-helper-batch/src/main/java/home/helper/batch/persistence/database/v1/imports/ImportMigrationUserSaveRepository.java
+++ home-helper-batch/src/main/java/home/helper/batch/persistence/database/v1/imports/ImportMigrationUserSaveRepository.java
@@ -5,14 +5,27 @@
 package home.helper.batch.persistence.database.v1.imports;
 
 import org.springframework.stereotype.Repository;
+import lombok.RequiredArgsConstructor;
 
 import home.helper.batch.dto.v1.imports.ImportMigrationUserOutput;
 import home.helper.batch.gateway.SaveGateway;
+import home.helper.batch.persistence.database.v1.table.DbV1User;
+import home.helper.batch.persistence.database.v1.table.DbV1UserMapper;
 
 @Repository
+@RequiredArgsConstructor
 public class ImportMigrationUserSaveRepository implements SaveGateway<ImportMigrationUserOutput> {
+    private final DbV1UserMapper dbV1UserMapper;
+
     @Override
     public void save(ImportMigrationUserOutput data) {
-        System.out.println(data);
+        dbV1UserMapper.insertSelective(
+            new DbV1User() {{
+                setUserId(data.userId());
+                setUserName(data.userName());
+                setDisplayOrder(data.displayOrder());
+                setEmail(data.email());
+            }}
+        );
     }
 }
