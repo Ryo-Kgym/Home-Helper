d13bb3f9b3d819d1a9e4d641184f49de48befedf
Ryo-Kgym <mraytoh@icloud.com>
2023-12-21 10:43:05 +0900

    Home-Helper-150 - ULID のライブラリをimportした。 - conv_idにULIDを登録するようにした。

--- home-helper-batch/src/main/java/home/helper/batch/persistence/database/converter/UlidGenerator.java
+++ home-helper-batch/src/main/java/home/helper/batch/persistence/database/converter/UlidGenerator.java
@@ -6,17 +6,17 @@ package home.helper.batch.persistence.database.converter;
 
 import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
+import de.huxhorn.sulky.ulid.ULID;
 
 import home.helper.batch.gateway.migration.MigrationIdGateway;
 
 @Component
 @RequiredArgsConstructor
 public class UlidGenerator implements MigrationIdGateway {
-
-    private int count = 0;
+    private final ULID ulid = new ULID();
 
     @Override
     public String generateId() {
-        return String.valueOf(count++);
+        return String.valueOf(ulid.nextULID());
     }
 }
